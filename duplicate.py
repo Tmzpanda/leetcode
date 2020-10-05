a5a763cdc2e4403cea8b61d0a0f2f00c00313fed
tmzpanda <tmzpanda@gmail.com>
2020-10-04 22:35:22 -0400

    Update duplicate.py

--- duplicate.py
+++ duplicate.py
@@ -1,8 +1,47 @@
 """
-duplication - sort    O(nlogn)
-            - set     O(n)  O(n)
-            - cycle detection   O(n)
-deduplication 
+duplication - sort    O(nlogn) O(1)
+            - set     O(n) O(n)
+            - value index mapping   O(n) O(1)
 
-linked list cycle
 """
+
+# sort 
+def findDuplicate(nums):
+    nums.sort()
+    for i in range(len(nums)):
+        if i > 0 and nums[i] == nums[i-1]:
+            return nums[i]
+
+
+# set 
+# deduplicate
+def deduplicate(nums):
+    index = 1
+    traversed = set()
+    for i in range(len(nums)):
+        if i > 0 and nums[i] not in traversed:
+            nums[index] = nums[i]
+            index += 1
+        traversed.add(nums[i])
+
+    return nums[:index]
+
+
+# value index mapping
+"""
+1 ≤ a[i] ≤ n 
+0 ≤ index ≤ n - 1
+<value, index> exclusive mapping makes O(n) O(1) achievable
+
+"""
+
+def findDuplicates(nums):
+    res = []
+    
+    for i in range(len(nums)):
+        index = abs(nums[i]) - 1
+        if nums[index] < 0:
+            res.append(abs(nums[i]))
+        nums[index] = - nums[index]
+        
+    return res
