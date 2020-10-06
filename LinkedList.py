ae005592b7ceb5fc169d9288c5a42bd499f8d831
tmzpanda <tmzpanda@gmail.com>
2020-10-05 23:12:41 -0400

    Create LinkedList.py

---
+++ LinkedList.py
@@ -0,0 +1,47 @@
+"""
+# merge
+# hasCycle
+
+
+"""
+
+
+
+# merge
+def merge(l1, l2): 
+    temp = dummy = ListNode(0)
+    p1, p2 = l1, l2
+    while p1 or p2:
+        if isFirstPointerSmaller(p1, p2):
+            temp.next = p1
+            p1 = p1.next
+        else:
+            temp.next = p2
+            p2 = p2.next
+        temp = temp.next
+
+    return dummy.next
+
+def isFirstSmaller(p1, p2):
+    if p1 is None:
+        return False
+    if p2 is None:
+        return True
+    return p1.val <= p2.val
+    
+ 
+# hasCycle
+def hasCycle(head):
+    if head is None:            
+        return False    
+    
+    p1 = head       
+    p2 = head       
+    while p1 != p2:
+        if p1 is None or p1.next is None:
+            return False
+        
+        p1 = p1.next.next
+        p2 = p2.next
+    
+    return True
