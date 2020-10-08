1d22e4a47409e44074cd9628207539cb5b7bc772
tmzpanda <tmzpanda@gmail.com>
2020-10-07 23:30:39 -0400

    Update subsequence.py

--- subsequence.py
+++ subsequence.py
@@ -1,10 +1,34 @@
 """
-# sell stock
+# subsequence
+# sell stock - greedy O(n)
+# LIS - dp O(n^2)
+# LCS
 
+
+# subset
+# kSum O(n^(k-1))
+# combinationSum - all possible solutions - backtrack O(2^n)
+                 - if possible solution exists - dp O(n*S)
+                 - number of possible solutions - dp O(n*S)
+           
 """
 
 
 
-
+# sell stock - greedy O(n)
+import sys
 def maxProfit(prices):
+    minPrice = sys.maxsize
+    profit = 0
+    
+    for price in prices:
+        if price - minPrice > profit:
+            profit = price - minPrice
+        if price < minPrice:
+            minPrice = price
+            
+    return profit
+ 
+
+# 
 
