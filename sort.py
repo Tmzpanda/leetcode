f694432f51c9e3f8634fa9b956ae58724b019ffa
tmzpanda <tmzpanda@gmail.com>
2020-10-07 23:30:35 -0400

    Create sort.py

---
+++ sort.py
@@ -0,0 +1,46 @@
+"""
+# partition
+# sort by parity - partition O(n)
+# sort colors
+
+# merge
+# kClosest O(logn + k)
+# intersection - O(m + n)
+               - O(m*logn)
+
+
+# absSort
+
+"""
+
+#********************************* partition **********************************************
+# sort by parity
+def sortArrayByParity(self, nums):
+    l, r = 0, len(nums) - 1
+
+    while l < r:
+        while l < r and nums[l] % 2 == 0:
+            l += 1
+        while l < r and nums[r] % 2 == 1:
+            r -= 1
+        if l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l += 1
+            r -= 1
+            
+    return nums
+    
+
+# sort color
+
+
+
+
+
+#********************************* selection sort **********************************************
+
+
+
+
+
+    
