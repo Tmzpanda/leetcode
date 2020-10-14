3d429a0044ec391fb5cb6856a32c73e6cef40d82
tmzpanda <tmzpanda@gmail.com>
2020-10-14 14:03:07 -0400

    Update maxtrix.py

--- maxtrix.py
+++ maxtrix.py
@@ -128,29 +128,27 @@ def uniquePaths(m, n):
 # Knight Probability in Chessboard - O(K*n^2)
 DIRECTIONS = [(-1, -2), (-2, -1), (-2, 1), (-1, 2), 
                (1, -2), (2, -1), (2, 1), (1, 2)]
-
-def knightProbability(N, K, r, c):
-
-    dp = [[0 for _ in range(N)] for _ in range(N)]
-    dp[r][c] = 1
-
-    for step in range(1, K + 1):
-        dpTemp = [[0 for i in range(N)] for j in range(N)]    
+               
+class Solution:
+    def knightProbability(self, N, K, r, c):
+        dp = [[0 for _ in range(N)] for _ in range(N)]
+        dp[r][c] = 1
+    
+        for step in range(K):
+            dpTemp = [[0 for i in range(N)] for j in range(N)]    
+            for i in range(N):
+                for j in range(N):
+                    for direction in DIRECTIONS:
+                        prev_i, prev_j = i - direction[0], j - direction[1]
+                        if 0 <= prev_i < N and 0 <= prev_j < N:
+                            dpTemp[i][j] += dp[prev_i][prev_j] * 0.125
+            dp = dpTemp
+    
+        res = 0
         for i in range(N):
             for j in range(N):
-                if dp[i][j]:    
-                    for direction in DIRECTIONS:
-                        next_i, next_j = i + direction[0], j + direction[1]
-                        if 0 <= next_i < N and 0 <= next_j < N:
-                            dpTemp[next_i][next_j] += dp[i][j] * 0.125
-        dp = dpTemp
-
-    res = 0
-    for i in range(N):
-        for j in range(N):
-            res += dp[i][j]
-    return res
-
+                res += dp[i][j]
+        return res
 
 
 
