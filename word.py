2447c684ea06252137808973c4f28c26ec7041ae
tmzpanda <tmzpanda@gmail.com>
2020-10-15 22:03:35 -0400

    Update word.py

--- word.py
+++ word.py
@@ -1,25 +1,120 @@
 """"
-
-# wordLadder - bfs
-             - dp
-
+# Word Ladder - bfs
+              - dp
 
 
-# wordBreak
+
+
+# Word Break - if possible solution exists
+             - number of possible solutions
+             - all possible solutions           
+# Decode Ways - dfs 
+              - dp
+# Word Pattern       
+# Wildcard Matching
+
+
+
+
+
+
+# Levenshtein distance
+
 
 
 """
+# Word Ladder - dp
 
 
 
 
 
 
+# *********************************************** Word Break **************************************************************
 
-
-
-# Decode Ways
+# Word Break - if solution exists - dp O(n^2)
 """
+    s     l e e t c o d e
+   dp   T       x = dp[i] and s[i to j-1] in wordSet    
+        i       j
+        
+"""
+def wordBreak(s, wordSet):
+       
+    n = len(s)
+    dp = [False for _ in range(n + 1)] 
+    dp[0] = True
+   
+    for i in range(n + 1):
+       for j in range(i + 1, n + 1):
+           dp[j] = dp[i] and s[i: j] in wordSet
+              
+    return dp[n]
+
+
+# Word Break - number of solutions - dp O(n^2)
+def wordBreak(s, dict):
+    n = len(s)
+    s = s.lower()
+    wordSet = set([word.lower() for word in dict])
+    
+    dp = [0 for _ in range(n + 1)]
+    dp[0] = 1   
+    for i in range(n + 1):
+        for j in range(i + 1, n + 1):
+            if s[i: j] in wordSet:
+                dp[j] += dp[i]     
+                
+    return dp[n]
+
+
+# Word Break - all solutions 
+"""
+                              "catsanddogs"
+                  cat "sanddogs"        cats "anddogs"    
+                    sand "dogs"               and "dogs"     
+                         dogs ""                   dogs ""
+
+"""
+class Solution:
+
+    def wordBreak(self, s, wordDict):
+        return self.dfs(s, wordDict, {})
+        
+    def dfs(self, s, wordDict, memo):
+
+        if s in memo:
+            return memo[s]
+        
+        partitions = []
+        if s in wordDict:
+            partitions.append(s)
+        
+        for i in range(1, len(s)):
+            prefix = s[:i]
+            if prefix not in wordDict:
+                continue
+            
+            sub_partitions = self.dfs(s[i:], wordDict, memo)
+            for partition in sub_partitions:
+                partitions.append(prefix + " " + partition)
+                
+        memo[s] = partitions    
+        return partitions
+
+
+
+# Decode Ways      
+# dp
+"""
+
+                                    "2263"
+                          2 "263"               22 "63" <-------------- same 
+    same  -------> 2 "63"        26 "3"             6 "3"
+                      6 "3"          3 ""              3 ""
+                         3 ""
+
+
 dp O(n)
 dp[i] = dp[i - 1]*(if "s[i - 1]" is valid) + dp[i - 2]*(if "s[i - 2]s[i - 1]" is valid)
 
