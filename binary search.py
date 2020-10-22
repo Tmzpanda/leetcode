46f5d37cc20b9cb2af69b613dd3ffbcff2299129
tmzpanda <tmzpanda@gmail.com>
2020-10-21 22:13:02 -0400

    Update binary search.py

--- binary search.py
+++ binary search.py
@@ -1,1 +1,34 @@
+"""
+# target
+# intersection 
+
+
+# insert/upper
+# kClosest
+# Russian Doll Envelopes
+
+
+
+# rotated
+
+
+# mountain 
+
+
+# bad version
+# 
+
+
+
+
+visulization:
+x x x x x x o
+      -
+      l     r
+        _
+        l   r
+          -
+          l r
+
+"""
 
