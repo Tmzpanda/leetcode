93109cb6987d14afb9d1146bc23615f3170c4766
tmzpanda <tmzpanda@gmail.com>
2020-11-20 13:29:05 -0500

    Update tree.py

--- tree.py
+++ tree.py
@@ -108,3 +108,16 @@ class Solution:
 
 
   
+
+
+
+# Invert a Binary Tree
+def invertTree(root):
+    if not root:
+        return
+    
+    self.invertTree(root.left)
+    self.invertTree(root.right)
+    root.left, root.right = root.right, root.left
+    
+    return root
