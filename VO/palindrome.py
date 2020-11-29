db8c401c5f716d0d679eb55e371ad11a6cbd0866
tmzpanda <tmzpanda@gmail.com>
2020-11-29 00:57:10 -0500

    Create palindrome.py

---
+++ VO/palindrome.py
@@ -0,0 +1,23 @@
+
+
+
+# Longest Common Substring
+
+# K-Palindrome
+# Longest Panlindrome Substring - LPS - two pointers
+                                      - DP
+                                - remove K characters
+                                      - dfs
+                                      - edit distance???
+                                - remove a substring
+# Longest Common Subsequence
+# Longest Palindrome Subsequence
+
+# Palindrome Partioning - all possible solutions
+                        - minimum cuts
+                        
+# Word Break
+
+# Subset
+# Permutation
+
