a78282f26b76eb6cc98061a06e5bfe36c30a7bd1
tmzpanda <tmzpanda@gmail.com>
2020-12-01 16:57:20 -0500

    Update subarray.py

--- VO/subarray.py
+++ VO/subarray.py
@@ -1,15 +1,45 @@
-# 53. Maximum Subarray - greedy O(n)
-# 560. Subarray with Sum Equals K - hashmap O(n)
-# 325. Longest Subarray with Sum Equals K - hashmap O(n)
+"""
+53. Maximum Subarray - greedy O(n)
+560. Subarray with Sum Equals K - hashmap O(n)
+325. Longest Subarray with Sum Equals K - hashmap O(n)
 
-# 209. Shortest Subarray with Sum at Least K - positive - sliding window O(n)
-# 862. Shortest Subarray with Sum at Least K - mono-queue O(n)
-#      Longest Subarray with Sum at Most K
+209. Shortest Subarray with Sum at Least K - positive - sliding window O(n)
+862. Shortest Subarray with Sum at Least K - mono-queue O(n)
+     Longest Subarray with Sum at Most K
 
-# 1438. Longest Subarray With Absolute Diff at most K - sliding window O(n)
-# 3. Longest Substring Without Repeating Characters - sliding window O(n)
-# 76. Minimum Window with Target Characters - sliding window O(n)
-# 727. Minimum Window with Target Subsequence
+1438. Longest Subarray With Absolute Diff at most K - sliding window O(n)
+3. Longest Substring Without Repeating Characters - sliding window O(n)
+76. Minimum Window with Target Characters - sliding window O(n)
+727. Minimum Window with Target Subsequence - two pointers O(ST) = O((# of pattern found)*S*T)
 
-# 674. Longest Increasing Subarray - greedy O(n)
-# 300. Longest Increasing Subsequence - dp O(n)
+674. Longest Increasing Subarray - greedy O(n)
+300. Longest Increasing Subsequence - dp O(n)
+
+
+
+718. Longest Common Substring - dp O(m*n)
+                              - two pointers O(m * n * min(m,n))
+1143. Longest Common Subsequence - dp O(m*n)
+5. Longest Panlindrome Substring - two pointers O(n^2)
+                                 - dp O(n^2)
+516. Longest Palindromic Subsequence - dp O(n^2)
+
+
+
+
+
+
+1216. K-Palindrome Substring - LPS
+                             - memoization
+                              
+131. Palindrome Partioning - all solutions - backtrack
+                           - minimum cuts - memoizaiton
+                           
+139. Word Break - number of solutions - memoization
+                - all solutions - memoization 
+                
+90. Subset
+47. Permutation
+KSum
+
+"""
