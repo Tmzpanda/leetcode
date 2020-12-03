39f2509450015c9103cd717d5e32e288b3962c9f
tmzpanda <tmzpanda@gmail.com>
2020-12-02 19:36:47 -0500

    Update subarray.py

--- VO/subarray.py
+++ VO/subarray.py
@@ -1,31 +1,45 @@
 """
 53. Maximum Subarray - greedy O(n)
-560. Subarray with Sum Equals K - hashmap O(n)
+560. Subarray with Sum Equals - K - hashmap O(n)
+974.                          - nK - hashmap O(n)
 325. Longest Subarray with Sum Equals K - hashmap O(n)
 
 209. Shortest Subarray with Sum at Least K - positive - sliding window O(n)
-862. Shortest Subarray with Sum at Least K - mono-queue O(n)
-     Longest Subarray with Sum at Most K
+862. - Shortest Subarray with Sum at Least K - mono-queue O(n)
+     - Longest Subarray with Sum at Most K
 
 1438. Longest Subarray With Absolute Diff at most K - sliding window O(n)
 3. Longest Substring Without Repeating Characters - sliding window O(n)
-76. Minimum Window with Target Characters - sliding window O(n)
-727. Minimum Window with Target Subsequence - two pointers O(ST) = O((# of pattern found)*S*T)
+76. Minimum Window with - Target Characters - sliding window O(n)
+727.                    - Target Subsequence - two pointers O(ST) = O((# of pattern found)*S*T)
 
 674. Longest Increasing Subarray - greedy O(n)
 300. Longest Increasing Subsequence - dp O(n)
 
 
+121. Sell Stock - Maximum Diff Sum - one transaction - greedy O(n)
+122.                               - ∞ transactions - 
+188.                               - at most K transactions - dp
+309.                               - ∞ transactions with cooldown - 
+714.                               - ∞ transactions with transaction fee -
+
+Knapsack - combinatorial optimization  
+198. Rob House - Non-adjacent Elements Sum - array
+213.                                       - cycle
+337.                                       - tree
+
+322. Coin Change - Combination Sum - all solutions
+518.                               - fewest coins
+
+
+
 
 718. Longest Common Substring - dp O(m*n)
-                              - two pointers O(m * n * min(m,n))
+                              - two pointers O(m * n * min(m,n)) TLE
 1143. Longest Common Subsequence - dp O(m*n)
 5. Longest Panlindrome Substring - two pointers O(n^2)
                                  - dp O(n^2)
 516. Longest Palindromic Subsequence - dp O(n^2)
-
-
-
 
 
 
@@ -40,6 +54,11 @@
                 
 90. Subset
 47. Permutation
-KSum
+39. KSum
+
+"""
+
+"""
+Distance 
 
 """
