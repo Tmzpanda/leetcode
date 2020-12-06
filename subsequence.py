a4f66783f1ee1f42ae0f84897607823384b90b08
tmzpanda <tmzpanda@gmail.com>
2020-12-06 18:53:27 -0500

    Update subsequence.py

--- subsequence.py
+++ subsequence.py
@@ -46,14 +46,13 @@ def maxProfit(prices):
 prices = [3,2,6,5,0,3] k = 2
 
   0 1 2
-3 0   #
-2 0   .
-6 0   . 
-5 0   .
-0 0   #
-3 0 # x = max(dp[i][j - 1],
-              max(prices[i] - prices[x] + dp[x][j - 1]), where 0 <= x <= i
-              )
+0 0 0 0
+3 0 0 0
+2 0 x = max(dp[i - 1][j],
+6 0         max(prices[i] - prices[x] + dp[x - 1][j - 1]), where 0 <= x < i
+5 0         )
+0 0
+3 0  
 
 """
 def maxProfit(k, prices):
@@ -74,9 +73,47 @@ def maxProfit(k, prices):
   
   
   
+# sell stock - cool down
+"""
+     3 2 6 5 0 3
+ 0 0 0 x = max(dp[i - 1],
+               prices[i] - prices[x] + dp[x - 2], where 0 <= x < i
+              )
+     
+"""
+import sys
+def maxProfit(prices):
+    n = len(prices)
+    dp = [0] * (n + 2)
+
+    profit = -sys.maxsize
+    for i in range(3, n + 2):
+        profit = max(profit, -prices[i - 2 - 1] + dp[i - 1 - 2])
+        dp[i] = max(dp[i - 1], prices[i - 2] + profit)
+
+    return dp[-1]
   
 
+# sell stock - transaction fee
+"""
+  1, 3, 2, 8, 4, 9
+0 0  x = max(dp[i - 1],
+             prices[i] - prices[x] - 2 + dp[x - 1], where 0 <= x < i
+             )
+  
+"""
+def maxProfit(prices, fee):
+        
+    n = len(prices)
+    dp = [0] * (n + 1)
 
+    profit = -sys.maxsize
+    for i in range(2, n + 1):
+        profit = max(profit, -prices[i - 1 - 1] - fee + dp[i - 1 - 1])
+        dp[i] = max(dp[i - 1], prices[i - 1] + profit)
+
+    print(dp)
+    return dp[-1]
 
 
 
