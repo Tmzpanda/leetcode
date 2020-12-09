45cd00af59fa646acc4cb22cd5c68562dabd2157
tmzpanda <tmzpanda@gmail.com>
2020-12-08 21:13:02 -0500

    Create palindrome.py

---
+++ summary/palindrome.py
@@ -0,0 +1,137 @@
+"""
+718. Longest Common Substring - dp O(m*n)
+                              - two pointers O(m * n * min(m,n)) TLE
+1143. Longest Common Subsequence - dp O(m*n)
+5. Longest Panlindrome Substring - two pointers O(n^2)
+                                 - dp O(n^2)
+516. Longest Palindromic Subsequence - dp O(n^2)
+
+1216. K-Palindrome Substring - LPS
+                             - dfs memoization - dp
+                             
+131. Palindrome Partioning - all solutions - backtrack
+132.                       - minimum cuts - memoizaiton - dp
+
+"""
+
+
+718. Longest Common Substring - dp O(m*n)
+                              - two pointers O(m * n * min(m,n)) TLE
+1143. Longest Common Subsequence - dp O(m*n)
+
+# Longest Common Substring O(n^2)
+def longestCommonSubstring(A, B):
+        
+    longest = 0
+    for i in range(len(A)):
+        for j in range(len(B)):
+
+            l = 0
+            while i + l < len(A) and j + l < len(B) and A[i + l] == B[j + l]:
+                longest = max(longest, l + 1)
+                l += 1
+
+    return longest
+
+
+# Longest Common Subsequence O(n^2)
+"""
+  "" A G G T A B
+"" 0 0 0 0 0 0 0 
+G  0 
+X  0
+T  0
+X  0
+A  0
+Y  0         x = max(dp[i - 1][j], dp[i][j - 1]), when A[i - 1] != B[j - 1]
+B  0           x = dp[i - 1][j - 1] + 1, when A[i - 1] == B[j - 1]
+"""
+def LCS(A, B):
+    if not A or not B:
+        return 0
+        
+    m, n = len(A), len(B)
+    dp = [[0] * (n + 1) for i in range(m + 1)]
+    
+    for i in range(1, m + 1):
+        for j in range(1, n +  1):
+            if A[i - 1] == B[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+    return dp[m][n]
+
+
+# Longest Panlindrome Substring
+# two pointers
+class Solution:
+  
+    def longestPalindromeSubstring(self, s):
+        if not s:
+            return ""
+
+        longest = ""
+        for i in range(len(s)):
+            sub1 = self.findPalindrome(s, i, i)
+            sub2 = self.findPalindrome(s, i, i + 1)
+            sub = max(sub1, sub2, key=lambda x: len(x))
+            if len(sub) > len(longest):
+                longest = sub
+
+        return longest
+    
+    def findPalindrome(self, string, l, r):
+        while l >= 0 and r < len(string) and string[l] == string[r]:
+            l -= 1
+            r += 1
+        
+        l, r = l + 1, r - 1
+        return string[l:r + 1]
+        
+# dp
+def longestPalindromeSubstring(s):
+    if not s:
+        return 0
+
+    n = len(s)
+    dp = [[False] * n for _ in range(n)]
+    
+    for i in range(n):
+        dp[i][i] = True
+    for i in range(1, n):
+        dp[i][i - 1] = True
+       
+    longest = 0
+    start, end = 0, 0
+    for length in range(2, n + 1):
+        for i in range(n - length + 1):
+            j = i + length - 1
+            dp[i][j] = s[i] == s[j] and dp[i + 1][j - 1]
+            
+            if dp[i][j]:
+                longest = length
+                start, end = i, j
+                
+    return s[start: end + 1]
+
+
+# Longest Panlindrome Subsequence
+def longestPalindromeSubseq(s):
+    if not s:
+        return 0
+
+    n = len(s)
+    dp = [[0] * n for _ in range(n)]
+    for i in range(n):
+        dp[i][i] = 1
+        
+    for length in range(2, n + 1):
+        for i in range(n - length + 1):
+            j = i + length - 1
+            if s[i] == s[j]:
+                dp[i][j] = dp[i + 1][j - 1] + 2
+            else:
+                dp[i][j] = max(dp[i + 1][j], dp[i][j - 1])
+         
+    return dp[0][n - 1]
