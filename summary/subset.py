632254f7b5244d6b72bcbba8736237df5307284d
tmzpanda <tmzpanda@gmail.com>
2020-12-08 21:04:26 -0500

    Create subset.py

---
+++ summary/subset.py
@@ -0,0 +1,27 @@
+"""
+121. Sell Stock - Subsequence Maximum Diffs - one transaction - greedy O(n)
+122.                                        - ∞ transactions - greedy O(n) 
+188.                                        - at most K transactions - dp O(n*k)
+309.                                        - ∞ transactions with cooldown - dp O(n)
+714.                                        - ∞ transactions with transaction fee - dp O(n)
+
+198. Rob House - Non-adjacent Elements Sum - array - dp O(n)
+213.                                       - cycle - dp O(n)
+337.                                       - tree - d&q O(n)
+
+322. Coin Change - Combination Sum - number of solutions - dp O(n*S)
+518.                               - fewest coins dp O(S*n)
+
+39. Combination Sum - all solutions - repeated use - backtrack O(2^n)
+40.                                 - deduplicate num exists - backtrack O(2^n)
+377.                - number of solutions - different sequences are different combinations - dp O(S*n)
+416. Partiton Equal Subset Sum - if possible - dp O(n*S)
+
+216. K Sum - positive - all solutions - backtrack O(2^n)
+                      - number of solutions - dp O(n*S*K)
+18.        - negative exists - all solutions - two pointers O(n^(k-1))
+
+47. Permutation - all solutions - duplicate num exists - backtrack O(n!)
+90. Subset -  all solutions - duplicate num exists - backtrack O(2^n)
+
+"""
