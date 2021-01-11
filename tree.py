782ff3fe241386c6c225a1d80eef79ec606184ac
Tim Zhang <tmzpanda@gmail.com>
2021-01-11 16:16:22 -0500

    Update tree.py

--- tree.py
+++ tree.py
@@ -14,6 +14,9 @@
 # K Smallest Values in a BST - O(k)
 # K Closest Values in a BST - O(logn + k)
 
+
+# serialization & deserialization 
+
 """
 
 
@@ -286,3 +289,90 @@ def kthSmallest(root, k):
 
 
 
+
+
+#****************************************** serialization **********************************************
+# serialization & deserialization 
+# bfs
+class Codec:
+
+    @staticmethod
+    def serialize(root):
+        if root is None:
+            return ""
+
+        # use bfs to serialize the tree
+        queue = deque([root])
+        bfs_order = []
+        while queue:
+            node = queue.popleft()
+            bfs_order.append(str(node.val) if node else '#')
+            if node:
+                queue.append(node.left)
+                queue.append(node.right)
+
+        while bfs_order[-1] == '#':
+            bfs_order.pop()
+
+        return "[%s]" % ','.join(bfs_order)  # "[8,3,10,1,6,#,14,#,#,4,7,13]"
+
+    @staticmethod
+    def deserialize(data):
+
+        if data == "[]":
+            return None
+
+        vals = data[1:-1].split(',')  # ['8', '3', '10', '1', '6', '#', '14', '#', '#', '4', '7', '13']
+        root = TreeNode(int(vals[0]))
+        queue = [root]
+        index = 0
+        is_left_child = True
+
+        for val in vals[1:]:
+            if val is not '#':
+                node = TreeNode(int(val))
+                if is_left_child:
+                    queue[index].left = node
+                else:
+                    queue[index].right = node
+                queue.append(node)
+
+            if not is_left_child:
+                index += 1
+
+            is_left_child = not is_left_child
+
+        return root
+    
+   
+# dfs
+class Codec:
+
+    def serialize(self, root):
+        def dfs(root):
+            if not root:
+                res.append("None,")
+                return 
+            res.append(str(root.val)+",")
+            dfs(root.left)
+            dfs(root.right)
+            
+        res = []
+        dfs(root)
+        return "".join(res)
+
+    def deserialize(self, data):
+        def helper(q):
+            if q[0] == "None":
+                q.popleft()
+                return
+            root = TreeNode(q.popleft())
+            l = helper(q)
+            r = helper(q)
+            root.left = l
+            root.right = r
+            return root
+        
+        lst = data.split(",")
+        q = collections.deque(lst)
+        return helper(q)
