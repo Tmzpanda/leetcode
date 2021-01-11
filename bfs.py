0f9e54331210fd4766bc05aa477514dd0f2f4151
Tim Zhang <tmzpanda@gmail.com>
2021-01-10 21:39:04 -0500

    Update bfs.py

--- bfs.py
+++ bfs.py
@@ -203,6 +203,50 @@ class Solution:
         return words
         
 
+# BFS
+DIRECTIONS = [(0, -1), (-1, 0), (0, 1), (1, 0)]
+
+class Solution:
+
+    def numIslands(self, grid):
+        if not grid or not grid[0]:
+            return 0
+            
+        res = 0
+        visited = set()
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if self.isValid(grid, i, j, visited):
+                    self.bfs(grid, i, j, visited)
+                    res += 1
+                    
+        return res                    
+    
+    def bfs(self, grid, x, y, visited):
+        queue = deque([(x, y)])
+        visited.add((x, y))
+        while queue:
+            x, y = queue.popleft()
+            for delta_x, delta_y in DIRECTIONS:
+                next_x = x + delta_x
+                next_y = y + delta_y
+                if not self.isValid(grid, next_x, next_y, visited):
+                    continue
+                queue.append((next_x, next_y))
+                visited.add((next_x, next_y))
+    
+    def isValid(self, grid, i, j, visited):
+            
+        n, m = len(grid), len(grid[0])
+        if not (0 <= i < n and 0 <= j < m):
+            return False
+        if (i, j) in visited:
+            return False
+        
+        return int(grid[i][j])
+
+
+  
         
 # All nodes Distance K in a Binary Tree - bfs O(n)
 from collections import deque
