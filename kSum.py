a7b9f13fbed6fd7288c9679435fdc612ff03865a
Tim Zhang <tmzpanda@gmail.com>
2021-01-16 23:40:18 -0500

    Update kSum.py

--- kSum.py
+++ kSum.py
@@ -1,66 +1,35 @@
 """
 
-# 2 sum - closest
-        - data structure
-        
-        
-        
-# One Possible Solution
 # 2Sum - hashmap
        - 2 pointers (if sorted)
-# 3Sum
-# 4Sum
-# kSum
-  
-  
-# All Possible Solutions - deduplicate
-# 4Sum 
-# kSum 
-# combinationSum 
+       
+# 2 sum - closest
+        - data structure
 
-39. Combination Sum - all solutions - repeated use - backtrack O(2^n)
-40.                                 - deduplicate num exists - backtrack O(2^n)
-377.                - number of solutions - different sequences are different combinations - dp O(n*S)
-416. Partiton Equal Subset Sum - if possible - dp O(n*S)
 
-216. K Sum - positive - all solutions - backtrack O(2^n)
-                      - number of solutions - dp O(n*S*K)
-18.        - negative exists - all solutions - two pointers O(n^(k-1))
+# 40. Combination Sum - all solutions - backtrack O(2^n)
+# 377. Combination Sum - number of solutions - dp O(S*n)
+# 518. Coin Change - number of solutions - dp O(n*S)
+# 416. Partiton Equal Subset Sum - if possible - dp O(n*S)
+# 322. Coin Change - fewest coins - dp O(S)
+# 47. Permutation - O(n!)
+
+
+# 216. K Sum - all positive - all solutions - backtrack O(2^n)
+# 18. K Sum - negative exists - all solutions - two pointers O(n^(k-1))
 
 
 """
 
-
-# 39 Combination Sum
+# ****************************************** Combination Sum **********************************************
+# 40. Combination Sum - all solutions - backtrack O(2^n)
 class Solution:
-    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
-        result = []
-        self.dfs(nums, 0, target, [], result)
-        return result
-        
-        
-    def dfs(self, nums, index, target, subset, result):
-        if target < 0:
-            return 
-        
-        if target == 0:
-            result.append(list(subset)) 
-            return
-        
-        for i in range(index, len(nums)):
-            subset.append(nums[i])
-            self.dfs(nums, i, target - nums[i], subset, result) # unique combinations, repeated use
-            subset.pop()
-
-# 40. Combination Sum
-class Solution:
-    def combinationSum2(self, nums: List[int], target: int) -> List[List[int]]:
+    def combinationSum2(self, nums, target):
         nums.sort()
         result = []
         self.dfs(nums, 0, target, [], result)
         return result
         
-        
     def dfs(self, nums, index, target, subset, result):
         if target < 0:
             return 
@@ -70,36 +39,97 @@ class Solution:
             return
         
         for i in range(index, len(nums)):
-            if i != 0 and nums[i] == nums[i - 1] and i != index:    # deduplicate
+            if i > 0 and nums[i] == nums[i - 1] and i != index:    # deduplicate
                 continue
             subset.append(nums[i])
             self.dfs(nums, i + 1, target - nums[i], subset, result)   # used once
+#             self.dfs(nums, i, target - nums[i], subset, result)     # repeated use
+#             self.dfs(nums, target - nums[i], subset, result)        # different sequences are counted as different combinations
             subset.pop()
 
 
-# 377. Combination Sum
+# 377. Combination Sum - number of solutions - dp O(S*n)
+def combinationSum(self, nums, target):
+        if not nums:
+            return 0
+        
+        dp = [0] * (target + 1)
+        dp[0] = 1
+        
+        for s in range(1, target + 1):
+            for num in nums:            # different sequences are counted as different combinations       
+                if num <= s:
+                    dp[s] += dp[s - num]
+        
+        return dp[target]
+
+
+# 518. Coin Change - number of solutions - dp O(n*S)
+# 416. Partiton Equal Subset Sum - if possible - dp O(n*S)
 class Solution:
-    def combinationSum4(self, nums: List[int], target: int) -> int:
+    def change(self, amount: int, coins: List[int]) -> int:
+        if amount == 0:
+            return 1
+        if not coins:
+            return 0
+        
+        n = len(coins)
+        dp = [[0 for _ in range(amount + 1)] for _ in range(n + 1)]
+        dp[0][0] = 1
+
+        for i in range(1, n + 1):
+            dp[i][0] = 1
+            for s in range(1, amount + 1):
+                if coins[i - 1] <= s:
+                    dp[i][s] = dp[i][s - coins[i -1]] + dp[i - 1][s]    # repeated use
+#                     dp[i][s] = dp[i - 1][s - coins[i -1]] + dp[i - 1][s]    # used once
+                else:
+                    dp[i][s] = dp[i - 1][s] 
+                    
+        return dp[-1][-1]
+
+
+# 322. Coin Change - fewest coins - dp O(S)
+def coinChange(coins, amount):
+    
+    dp = [sys.maxsize for i in range(amount + 1)]
+    dp[0] = 0
+    
+    for s in range(1, amount + 1):
+        for coin in coins:
+            if coin <= s:
+                dp[s] = min(dp[s], dp[s - coin] + 1)        # dp[i][s] = min(dp[i][s - coins[i -1]] + 1, dp[i - 1][s]) if coins[i - 1] <= s else dp[i - 1][s]
+    
+    return dp[amount] if dp[amount] != sys.maxsize else -1
+
+
+              
+# 47. Permutation - O(n!)
+class Solution:
+    def permutation(self, nums):
+        nums.sort()
+        used = [False] * len(nums)
         result = []
-        self.dfs(nums, target, [], result)
-        return len(result)
+        self.dfs(nums, used, [], result)
+        return result
         
-        
-    def dfs(self, nums, target, subset, result):
-        if target < 0:
+    def dfs(self, nums, used, path, result):
+        if len(path) == len(nums):
+            result.append(list(path)) 
             return 
         
-        if target == 0:
-            result.append(list(subset)) 
-            return
-        
         for i in range(len(nums)):
-            subset.append(nums[i])
-            self.dfs(nums, target - nums[i], subset, result) # different sequence are counted as different combinations
-            subset.pop()
+            if used[i] or (i > 0 and nums[i] == nums[i - 1] and not used[i - 1]):       # deduplicate
+                continue
+                
+            used[i] = True
+            self.dfs(nums, used, path + [nums[i]], result)   
+            used[i] = False
+        
+        
 
-
-# 216. K Sum
+# ****************************************** K Sum **********************************************
+# 216. K Sum - all positive - all solutions - backtrack O(2^n)
 class Solution:
     def combinationSum3(self, k: int, target: int) -> List[List[int]]:
        
@@ -121,157 +151,8 @@ class Solution:
             subset.pop()
         
         
-# 90. Subset 
-class Solution:
-    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
-        result = []
-        self.dfs(nums, 0, [], result)
-        return result
-        
-        
-    def dfs(self, nums, index, subset, result):
- 
-        result.append(list(subset)) 
-        
-        for i in range(index, len(nums)):
-            if i != 0 and nums[i] == nums[i - 1] and i != index:    # deduplicate
-                continue
-            subset.append(nums[i])
-            self.dfs(nums, i + 1, subset, result)   # used once
-            subset.pop()
-        
-              
-# 47. Permutation
-class Solution:
-    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
-        used = [False] * len(nums)
-        result = []
-        self.dfs(nums, used, [], result)
-        return result
-        
-    def dfs(self, nums, used, path, result):
-        if len(path) == len(nums):
-            result.append(list(path)) 
-            return 
-        
-        for i in range(len(nums)):
-            if used[i] or (i > 0 and nums[i] == nums[i - 1] and not used[i - 1]):
-                continue
-            used[i] = True
-            self.dfs(nums, used, path + [nums[i]], result)   # used once
-            used[i] = False
-       
-       
- # 60. Kth Permutation Sequence
-
-
-
-# *********************************** One Possible Solution ***************************************
-# two sum
-# hashmap O(n) O(n) 
-def twoSum(nums, target):
-    num_to_index = {}
-    for index, num in enumerate(nums):
-        if target - num in num_to_index:
-            return [num_to_index[target - num], index]
-        num_to_index[num] = index
-   
-   
-# two pointers - if sorted O(n) O(1)
-def twoSum(nums, target):
-    nums.sort()
-    l, r = 0, len(nums) - 1
-    while l < r:
-        if nums[l] + nums[r] == target:
-            return [l, r]
-        elif nums[l] + nums[r] < target:
-            l += 1
-        else:
-            r -= 1
-    return -1
-
-
-""""
-sort array
-arr = [2, 7, 4, 0, 9, 5, 1, 3], s = 20
-       i           i
-          j           j                  (arr[i], arr[j], where i < j)
-            [4, 0, 9, 5, 1, 3], s = 11   (complementing pair in the subarray - twoSum)
-            
-""""
+# 18. K Sum - negative exists - all solutions - two pointers O(n^(k-1))
 # four sum O(n^3)
-def fourSum(arr, s):
-    arr.sort()
-    n = len(arr)
-    
-    for i in range(n - 3):
-        for j in range(i + 1, n - 2):
-            target = s - (arr[i] + arr[j])
-            
-            # two sum
-            l, r = j + 1, n - 1
-            while l < r:
-                if arr[l] + arr[r] == target:
-                    return [arr[i], arr[j], arr[l], arr[r]]
-                elif arr[l] + arr[r] > target:
-                    r -= 1
-                else:
-                    l += 1       
-    return []
-
-
-# k sum O(n^(k-1))
-def kSum(nums, target, k):
-    nums.sort()
-    
-    results = []
-    dfs(nums, target, k, [], results)
-    return results[0]
-
-def dfs(nums, target, k, subset, results):
-    
-    if len(results) == 1:         # break
-        return
-
-    if k == 2:
-        l, r = 0, len(nums) - 1 
-        while l < r:
-            if nums[l] + nums[r] == target:
-                results.append(subset + [nums[l], nums[r]])
-                l += 1
-                r -= 1
-                while l < r and nums[l] == nums[l - 1]:        # deduplicate
-                    l += 1
-                while r > l and nums[r] == nums[r + 1]:
-                    r -= 1
-            elif nums[l] + nums[r] < target:
-                l += 1
-            else:
-                r -= 1
-        return
-       
-    for i in range(0, len(nums) - k + 1):  
-        if target < nums[i] * k or target > nums[-1] * k:      # trim 
-            break
-
-        if i > 0 and nums[i-1] == nums[i]:                     # deduplicate
-            continue
-         
-        subset.append(nums[i])
-        dfs(nums[i + 1:], target - nums[i], k - 1, subset, results)
-        subset.pop()
-        
-        
-arr = [2, 7, 4, 0, 9, 5, 1, 3, 3]
-s = 20
-k = 4
-kSum(arr, s, k)
-
-
-# *********************************** All Possible Solutions ****************************************
-# four sum 
 def fourSum(nums, target):
     nums.sort()
     res = []
@@ -290,6 +171,7 @@ def fourSum(nums, target):
                     res.append([nums[i], nums[j], nums[l], nums[r]])
                     r -= 1
                     l += 1
+                
                     while l < r and nums[l] == nums[l - 1]:         # deduplicate
                         l += 1
                     while l < r and nums[r] == nums[r + 1]:
@@ -298,10 +180,11 @@ def fourSum(nums, target):
                     r -= 1
                 else:
                     l += 1
+                
     return res
 
 
-# k sum
+# K Sum O(n^(k-1))
 def kSum(nums, target, k):
     nums.sort()
     
@@ -340,40 +223,6 @@ def dfs(nums, target, k, subset, results):
         subset.pop()
         
         
-arr = [2, 7, 4, 0, 9, 5, 1, 3, 3]
-s = 20
-k = 4
-kSum(arr, s, k)
-
-
-# Combination Sum - O(2^n)
-class Solution:
-
-    def combinationSum(self, nums, target):
-        nums.sort()
-        
-        results = []
-        self.dfs(nums, 0, target, [], results)
-        return results
-
-    def dfs(self, nums, index, target, subset, results):
-        if target == 0:
-            results.append(list(subset))                # deep copy
-            return
-
-        if target < 0:
-            return
-        
-        for i in range(index, len(nums)):
-            if i > index and nums[i] == nums[i - 1]:    # deduplicate
-                continue
-            
-            subset.append(nums[i])
-            self.dfs(nums, i + 1, target - nums[i], subset, results)
-            subset.pop()
 
 
 
-
-
-  
