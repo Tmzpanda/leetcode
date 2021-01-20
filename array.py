56325ed7f2ddd63951688af276258e91ae71a973
Tim Zhang <tmzpanda@gmail.com>
2021-01-19 23:13:13 -0500

    Create array.py

---
+++ array.py
@@ -0,0 +1,23 @@
+# merge 
+
+# partition
+
+
+# two pointers
+def intersection(nums1, nums2):
+    nums1.sort()
+    nums2.sort()
+    res = []
+
+    i, j = 0, 0
+    while i < len(nums1) and j < len(nums2):
+        if nums1[i] < nums2[j]:
+            i += 1
+        elif nums1[i] > nums2[j]:
+            j += 1
+        else:
+            res.append(nums1[i])
+            i += 1
+            j += 1
+
+    return res
