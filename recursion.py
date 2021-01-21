7d2c226bcdf5c566ac4d4ca2e58dfe99334f5e78
Tim Zhang <tmzpanda@gmail.com>
2021-01-21 17:23:33 -0500

    Update recursion.py

--- recursion.py
+++ recursion.py
@@ -1,58 +1,17 @@
 """
-# top-down
-# dfs memoization 
-# d&q
-
-
-# bottom-up
-# backtrack
+memoization 
+# 139. Word Break - if possible - dp O(n^2)
+                  - all solutions - dfs memoization
 
 
 
-# comparison
-# Word Break - dfs memoization 
-# Word Ladder - backtrack
-
-
-                                    "2263"
-                          2 "263"               22 "63" <-------------- same 
-    same  -------> 2 "63"        26 "3"             6 "3"
-                      6 "3"          3 ""              3 ""
-                         3 ""
-
-
-        26*len(s)
-        x 
-        x
-hit -> hot -> dot -> dog -> cog
-        o  ->  o ->   o  -> cog
-        .      
-        .      
-        o  ->  o ->   o  ->  o -> o -> cog  
-        x
-
-
-
-
-
+backtrack
+# 126. Word Ladder - all possible solutions - bfs + dfs
 
 """
-#******************************************** top-down ***************************************************
-# Word Break - O(n^2)
-"""
-                              "catsanddogs"
-                  cat "sanddogs"        cats "anddogs"    
-                    sand "dogs"               and "dogs"     
-                         dogs ""                   dogs ""
 
-
-dp O(n^2)
-    s     l e e t c o d e
-   dp   T       x = dp[i] and s[i to j-1] in wordSet    
-        i       j
-        
-"""
-# if solution exists
+# ********************************************* memoization **********************************************************
+# 139. Word Break - if possible - dp O(n^2)
 class Solution:
 
     def wordBreak(self, s, wordDict):
@@ -66,100 +25,124 @@ class Solution:
         if not s:
             return True
         
-        flag = False
+        res = False
         for i in range(0, len(s)):
             prefix = s[:i + 1]
             if prefix not in wordDict:
                 continue
 
             if self.dfs(s[i + 1:], wordDict, memo):
-                flag = True
+                res = True
                 
-        memo[s] = flag      
-        return flag
+        memo[s] = res      
+        return res
 
 
-# number of solutions
+# 139. Word Break - all solutions - dfs memoization O(n^2)
+#                                 - backtrack O(2^n) TLE
 class Solution:
-
     def wordBreak(self, s, wordDict):
         return self.dfs(s, wordDict, {})
         
-
     def dfs(self, s, wordDict, memo):
         if s in memo:
             return memo[s]
         
-        if not s:
-            return 1
+        partitions = []
+        if s in wordDict:
+            partitions.append(s)
         
-        res = 0
-        for i in range(0, len(s)):
+        # recursion
+        for i in range(len(s)): 
             prefix = s[:i + 1]
             if prefix not in wordDict:
                 continue
+            
+            sub_partitions = self.dfs(s[i + 1:], wordDict, memo)
+            for partition in sub_partitions:
+                partitions.append(prefix + " " + partition)
+                
+        memo[s] = partitions    
+        return partitions
+ 
 
-            res += self.dfs(s[i + 1:], wordDict, memo)
-                      
-        memo[s] = res   
-        return res
-
-
-
-# Decode Ways O(n)
-"""         
-dfs memoization
-
-                                    "2263"
-                          2 "263"               22 "63" <-------------- same 
-    same  -------> 2 "63"        26 "3"             6 "3"
-                      6 "3"          3 ""              3 ""
-                         3 ""
-
-
-
-dp O(n)
-            s   2 2 6 3
-           dp 1 1 x        = dp[i - 1]*(if "s[i - 1]" is valid) + dp[i - 2]*(if "s[i - 2]s[i - 1]" is valid)
-
-"""
+# backtrack TLE
 class Solution:
-    def numDecodings(self, s):
-        if not s or s[0] == '0':
-            return 0
-
-        return self.dfs(s, {})
-
-
-    def dfs(self, s, memo):
-        if s in memo:
-            return memo[s]
+    def wordBreak(self, s, wordDict):
+        result = []
+        self.dfs(s, [], result, wordDict)
+        return result
+        
+    def dfs(self, s, combination, result, wordDict):
 
         if not s:
-            return 1
+            result.append(" ".join(combination))
+        
+        for i in range(0, len(s)): 
+            prefix = s[:i + 1]
+            
+            if prefix in wordDict:              # backtrack
+                combination.append(prefix)
+                self.dfs(s[i + 1:], combination, result, wordDict)
+                combination.pop()
+                
+                
+                
 
-        res = 0
-        for i in (1, 2):
-            if len(s[:i]) == i and self.isValid(s[:i]):
-                res += self.dfs(s[i:], memo)
+# ********************************************* backtrack **********************************************************
+# 126. Word Ladder - all possible solutions - bfs + dfs
+from collections import deque
+class Solution:
+    
+    def findLadders(self, start, end, dict):
+        dictionary = set(dict)
+        dictionary.add(start)
+        dictionary.add(end)
 
-        memo[s] = res
-        return res
+        word_to_distance = self.bfs(end, dictionary)    # build graph
+        result = []
+        self.dfs(start, end, word_to_distance, [start], result, dictionary)     # backtrack
+        return result 
 
     
-    def isValid(self, s):
-        n = len(s)
-        num = int(s)
-        if n == 1 and 1 <= num <= 9:
-            return True
-        if n == 2 and 10 <= num <= 26:
-            return True
+    def bfs(self, start, dictionary):
+        queue = deque([start])
+        word_to_distance = {start: 0}
 
-        return False
+        while queue:
+            word = queue.popleft()
+            for next_word in self.get_next_words(word, dictionary):
+                if next_word not in word_to_distance:
+                        queue.append(next_word)
+                        word_to_distance[next_word] = word_to_distance.get(word, 0) + 1
 
+        return word_to_distance
+        
 
+    def get_next_words(self, word, dictionary):
+        words = []
+        for i in range(len(word)):
+            left, right = word[:i], word[i + 1:]
+            for char in "abcdefghijklmnopqrstuvwxyz":
+                next_word = left + char + right
+                if next_word != word and next_word in dictionary:
+                    words.append(next_word)
+    
+        return words
+        
+        
+    def dfs(self, word, end, word_to_distance, path, result, dictionary):
+        
+        if word == end:
+            result.append(path[:])
+            return
 
+        for next_word in self.get_next_words(word, dictionary):
+            if word_to_distance[next_word] != word_to_distance[word] - 1:  # shortest
+                continue
+            path.append(next_word)
+            self.dfs(next_word, end, word_to_distance, path, result, dictionary)
+            path.pop()
 
-
-
+        
 
