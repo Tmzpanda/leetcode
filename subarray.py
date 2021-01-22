f3e06920cffc748cadc09f2a0c41361a9c178e94
Tim Zhang <tmzpanda@gmail.com>
2021-01-22 12:44:44 -0500

    Update subarray.py

--- subarray.py
+++ subarray.py
@@ -1,84 +1,17 @@
 """
-# subarrays O(n^2)
-# Longest Increasing Subarray - 1d increasing/monotonous - greedy O(n)
-                              - 2d matrix - dfs memoization
-# Longest Palindrome Substring - middle out O(n^2)
-                               - dp O(n^2)
-      
-      
-      
-# subarraySum - prefix sum
-# Maximum Subarray - greedy O(n)
-# Subarray Sum Equals K - hashmap O(n)
+329. Longest Increasing Subarray - 2d - dp   ????????
 
-# Shortest Subarray with Sum at Least K - sliding window O(n)
-# Minimum Window Substring - two pointers O(S + T)
-# Minimum Window Subsequence - dp O(ST)
-                             - two pointers O(ST) = O((# of pattern found)*S*T)
+718. Longest Common - Substring - dp O(m*n)
+1143.               - Subsequence - dp O(n^2)   
+5. Longest Palindromic - Substring - 2pointers O(n^2)                  
+516.                   - Subsequence - dp O(n^2)
 
+
+121. Sell Stock - Subsequence Maximum Diffs - one transaction - greedy O(n)
 
 
 """
 
-  
-
-#************************************************************ Longest Subarray ********************************************************************
-# subarrays O(n^3)
-def findSubarrays(nums):
-    n = len(nums)
-    subarrays =  []
-    
-    for i in range(n):
-        for l in range(i, n):
-            subarray = nums[i, l + 1]
-        subarrays.append(subarray)
-                
-    return subarrays
-                
-nums = [1, 2, 3, 4, 5]
-findSubarray(nums)
-
-
-
-# Longest Increasing Subarray - greedy O(n)
-def LIS(nums):
-    if not nums:
-        return 0
-    
-    longest = 1
-    flag = 0
-    for i in range(1, len(nums)):
-        if nums[i] < nums[i - 1]: 
-            flag = i
-        longest = max(longest, i - flag + 1)
-        
-    return longest
-
-  
-# Longest Monotonous Subarray 
-def LIS(nums):
-    if not nums:
-        return 0
-    
-    longest = 1
-    increasingFlag, decreasingFlag = 0, 0
-    for i in range(1, len(nums)):
-        if nums[i] < nums[i - 1]: 
-            increasingFlag = i
-
-        elif nums[i] > nums[i - 1]:
-            decreasingFlag = i
-               
-        elif nums[i] == nums[i - 1]:
-            increasingFlag = i
-            decreasingFlag = i
-        
-        longest = max(longest, i - increasingFlag + 1, i - decreasingFlag + 1)
-
-    return longest
-
- 
- 
 # Longest Increasing Subarray 2d
 # dfs memoization
 """
@@ -193,91 +126,12 @@ def LPS(s):
       return s[start:end + 1]
 
 
-#*********************************************************** Subarray Sum *************************************************************************
 
-#*********************************************************** Minimum Window ************************************************************************
-# Shortest Subarray with Sum at Least K - sliding window O(n)
-"""
-A =       [1, 3, -1, 3, 1]    K = 5
-psum = [0, 1, 4,  3, 6, 7]
 
-queue [0, 1, 4]
-      [0, 1, 3]
-      [0, 1, 3, 6]
-         [1, 3, 6]   shortest = i - queue.popleft()
-            [3, 6, 7]
-"""
-import sys
-from collections import deque
-
-def shortestSubarray(A, K):
-    n = len(A)
-    psum = [0] * (n + 1)
-    
-    for i in range(1, n + 1):
-        psum[i] = psum[i - 1] + A[i - 1] 
-    
-    queue = deque()
-    shortest = sys.maxsize
-    for i, s in enumerate(psum):
-        while queue and s <= psum[queue[-1]]:
-            queue.pop()
-        
-        while queue and  s - psum[queue[0]] >= K:
-            shortest = min(shortest, i - queue.popleft())
-            
-        queue.append(i)
-        
-    return shortest if shortest != sys.maxsize else -1
             
 
 
-# Minimum Window Substring
-"""
-S = "azjskfzs"      T = "sz"
-     -  -      expand
-      - -      contract 
-       --   
-       - -
-"""
-# two pointers O(S + T)
-class Solution:
-    def minWindow(self, s, t):
-        targetCharToFreq = self.buildCharToFreq(t)
-        sourceCharToFreq = {}
-        matched, n = 0, len(targetCharToFreq)
-        
-        window = ""
-        minLen = sys.maxsize
-        l, r = 0, 0
-        for r in range(len(s)):
-            sourceCharToFreq[s[r]] = sourceCharToFreq.get(s[r], 0) + 1
-            if s[r] in targetCharToFreq and sourceCharToFreq[s[r]] == targetCharToFreq[s[r]]:
-                    matched += 1
 
-            while matched == n and l <= r: #
-                windowLen = r - l + 1
-                if windowLen < minLen:
-                    minLen = windowLen
-                    window = s[l: r + 1]
-
-                sourceCharToFreq[s[l]] -= 1
-                if s[l] in targetCharToFreq and sourceCharToFreq[s[l]] < targetCharToFreq[s[l]]:
-                    matched -= 1
-                    
-                l += 1      
-
-        return window
-
-        
-    def buildCharToFreq(self, s):
-        targetCharToFreq = {}
-        for c in s:
-            targetCharToFreq[c] = targetCharToFreq.get(c, 0) + 1
-        return targetCharToFreq
-        
-  
-  
 
 # Minimum Window Subsequence 
 # dp - O(S*T)
@@ -354,8 +208,151 @@ def minWindow(S, T):
         
 
   
+# sell stock - greedy O(n)
+import sys
+def maxProfit(prices):
+    minPrice = sys.maxsize
+    maxProfit = 0
+    
+    for price in prices:
+        maxProfit = max(price - minPrice, maxProfit)
+        minPrice = min(price, minPrice)
+            
+    return maxProfit
 
+
+# sell stock - at most K transactions - O(n^2 * k)
+"""
+prices = [3,2,6,5,0,3] k = 2
+
+  0 1 2
+0 0 0 0
+3 0 0 0
+2 0 x = max(dp[i - 1][j],
+6 0         max(prices[i] - prices[x] + dp[x - 1][j - 1]), where 0 <= x < i
+5 0         )
+0 0
+3 0  
+
+"""
+def maxProfit(k, prices):
+    if len(prices) <= 1:
+        return 0
+    
+    n = len(prices)
+    dp = [[0] * (k + 1) for _ in range(n)]
+    
+    for j in range(1, k + 1):
+        profit = -sys.maxsize
+        
+        for i in range(1, n):
+            profit = max(profit, -prices[i - 1] + dp[i - 1][j - 1])
+            dp[i][j] = max(dp[i - 1][j], prices[i] + profit)
+
+    return dp[-1][-1]
   
+  
+  
+# sell stock - cool down
+"""
+     3 2 6 5 0 3
+ 0 0 0 x = max(dp[i - 1],
+               prices[i] - prices[x] + dp[x - 2], where 0 <= x < i
+              )
+     
+"""
+import sys
+def maxProfit(prices):
+    n = len(prices)
+    dp = [0] * (n + 2)
+
+    profit = -sys.maxsize
+    for i in range(3, n + 2):
+        profit = max(profit, -prices[i - 2 - 1] + dp[i - 1 - 2])
+        dp[i] = max(dp[i - 1], prices[i - 2] + profit)
+
+    return dp[-1]
+  
+
+# sell stock - transaction fee
+"""
+  1, 3, 2, 8, 4, 9
+0 0  x = max(dp[i - 1],
+             prices[i] - prices[x] - 2 + dp[x - 1], where 0 <= x < i
+             )
+  
+"""
+def maxProfit(prices, fee):
+        
+    n = len(prices)
+    dp = [0] * (n + 1)
+
+    profit = -sys.maxsize
+    for i in range(2, n + 1):
+        profit = max(profit, -prices[i - 1 - 1] - fee + dp[i - 1 - 1])
+        dp[i] = max(dp[i - 1], prices[i - 1] + profit)
+
+    print(dp)
+    return dp[-1]
+
+
+
+
+
+  # 198. House Robber - array        
+class Solution:
+    def rob(self, nums):
+        if not nums:
+            return 0
+        if len(nums) <= 2:
+            return max(nums)
+
+        n = len(nums)
+        dp = [0 for _ in range(n + 1)]
+        dp[1], dp[2] = nums[0], max(nums[0], nums[1])
+
+        for i in range(3, n + 1):
+            dp[i] = max(nums[i - 1] + dp[i - 2], dp[i - 1])
+
+        return dp[n]
+
+# 213. House Robber - cycle
+class Solution:
+    def rob(self, nums):
+        if not nums:
+            return 0
+        if len(nums) <= 2:
+            return max(nums)
+
+        n = len(nums)
+        dp1, dp2 = [0 for _ in range(n)], [0 for _ in range(n)]
+        dp1[1], dp1[2] = nums[0], max(nums[0], nums[1])     # nums[0: n -2]
+        dp2[1], dp2[2] = nums[1], max(nums[1], nums[2])     # nums[1n -1]
+
+        for i in range(3, n):
+            dp1[i] = max(nums[i - 1] + dp1[i - 2], dp1[i - 1])
+            dp2[i] = max(nums[i] + dp2[i - 2], dp2[i - 1])
+
+        return max(dp1[n - 1], dp2[n - 1])
+
+      
+# 337. tree
+class Solution:
+    def rob(self, root):
+        rob, not_rob = self.visit(root)
+        return max(rob, not_rob)
+        
+    def visit(self, root):
+        if root is None:
+            return 0, 0
+        
+        left_rob, left_not_rob = self.visit(root.left)
+        right_rob, right_not_rob = self.visit(root.right)
+        
+        rob = root.val + left_not_rob + right_not_rob
+        not_rob = max(left_rob, left_not_rob) + max(right_rob, right_not_rob)
+        return rob, not_rob
+      
 
 
 
