eaeb5b5a8cda2e20add5b5f5e9901df463348895
Tim Zhang <tmzpanda@gmail.com>
2021-01-22 15:55:44 -0500

    Update recursion.py

--- recursion.py
+++ recursion.py
@@ -3,6 +3,8 @@ memoization
 # 139. Word Break - if possible - dp O(n^2)
                   - all solutions - dfs memoization
 # 236. LCA of a Binary Tree - d&q O(n) O(n)(recursion stack)
+# 329. Longest Increasing Subarray - 2d - dfs memoization
+
 
 
 
@@ -91,8 +93,43 @@ class Solution:
                 self.dfs(s[i + 1:], combination, result, wordDict)
                 combination.pop()
                 
+                   
+                  
+# 329. Longest Increasing Subarray - 2d - dfs memoization
+DIRECTIONS = [(0, -1), (-1, 0), (0, 1), (1, 0)]
+class Solution:
+    def longestIncreasingPath(self, matrix):
+        if not matrix:
+            return 0
+        
+        n, m = len(matrix), len(matrix[0])
+        record = [[1 for _ in range(m)] for _ in range(n)]
+        memo = {}
+        longest = 0
+        for i in range(n):
+            for j in range(m):
+                record[i][j] = self.dfs(matrix, i, j, memo)
                 
-                
+        return max(map(max, record))
+        
+    
+    def dfs(self, matrix, x, y, memo):
+        if (x, y) in memo:
+            return memo[(x, y)]
+        
+        longest = 1
+        for delta_x, delta_y in DIRECTIONS:
+            x_next, y_next = x + delta_x, y + delta_y
+            if self.isValid(matrix, x_next, y_next) and matrix[x][y] < matrix[x_next][y_next]:
+                longest = max(longest, self.dfs(matrix, x_next, y_next, memo) + 1)
+            
+        memo[(x, y)] = longest
+        return longest
+        
+        
+    def isValid(self, matrix, x, y):
+        return 0 <= x < len(matrix) and 0 <= y < len(matrix[0])
+      
 
 # ********************************************* backtrack **********************************************************
 # 126. Word Ladder - all possible solutions - bfs + dfs
