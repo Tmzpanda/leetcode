06aac13abbba1fcf13573eb6484811b82a59f228
Tim Zhang <tmzpanda@gmail.com>
2021-01-25 10:40:04 -0500

    Update tree.py

--- tree.py
+++ tree.py
@@ -4,10 +4,13 @@
 # 235. LCA in BST - iteration O(logn)
 # 236. LCA in Binary Tree - d&q O(n) O(n)(recursion stack)
 # 578. LCA in Binary Tree - may not exist - d&q O(n) O(n)
-
+# 112. Binary Tree Path Sum - if exists
+# 437.          Subpath Sum - number of solutions - psum
+            
 
 # traverse 
-# 113. Binary Tree Path Sum  - all solutions
+# 257. Binary Tree Paths - all solutions  
+# 113. Binary Tree Path Sum - all solutions
 # 272. K Closest BST Values - inorder traverse O(n)
                             - iterator O(logn + k)              
 # 297. Serialize and Deserialize Binary Tree - dfs
@@ -119,8 +122,93 @@ class Solution:
 
         return a, b, None 
 
+
+# 112. Binary Tree Path Sum - if exists
+class Solution:
+    def hasPathSum(self, root: TreeNode, target: int) -> bool:
+        if not root:
+            return False
+        
+        return self.dfs(root, target)
     
+    def dfs(self, node, target):
+        if not node:
+            return target == 0
+            
+        if target < 0:
+            return False
+            
+        return self.dfs(node.left, target - node.val) or self.dfs(node.right, target - node.val)
+      
+      
 #****************************************** traverse ***************************************************    
+# 257. Binary Tree Paths - all solutions
+class Solution:
+    def binaryTreePaths(root):
+        if root is None:
+            return []
+        
+        res = []
+        self.dfs(root, [str(root.val)], res)
+        return res
+    
+    def dfs(self, node, path, res):
+        if not node.left and not node.right:
+            res.append('->'.join(path))
+            return
+        
+        if node.left:
+            path.append(str(node.left.val))
+            self.dfs(node.left, path, res)      # backtrack 
+            path.pop() 
+        
+        if node.right:
+            path.append(str(node.right.val))
+            self.dfs(node.right, path, res)
+            path.pop()   
+            
+ 
+# 113. Binary Tree Path Sum - all solutions
+class Solution:
+    def pathSum(self, root: TreeNode, target: int) -> List[List[int]]:
+        res = []
+        self.dfs(root, target, [], res)
+        
+        return res
+    
+    def dfs(self, node, target, path, res):
+        if not node:
+            return 
+            
+        if target == node.val and node.left is None and node.right is None:
+            res.append(list(path + [node.val]))
+            return
+            
+        path.append(node.val)
+        self.dfs(node.left, target - node.val, path, res)
+        self.dfs(node.right, target - node.val, path, res)    # backtrack
+        path.pop()
+        
+
+# 272. K Closest BST Values 
+# recursion O(n)
+def kClosestValues(root, target, k):
+    res = deque()
+
+    def rec(root):        # traverse
+        if root is None:
+            return
+        rec(root.left)
+        if len(res) == k:
+            if not abs(target - root.val) < abs(target - res[0]):
+                return
+            res.popleft()
+        res.append(root.val)
+        rec(root.right)
+
+    rec(root)
+    return list(res)
+  
 # 297. Serialize and Deserialize Binary Tree - bfs
 #                                            - dfs
 
@@ -206,184 +294,4 @@ class Codec:
         lst = data.split(",")
         q = collections.deque(lst)
         return helper(q)
-    
-    
-    
- 
-
-
-
-
-
-
-# ***************************************** BST iterator **********************************************    
-# 230. Kth Smallest Element in a BST - iterator O(k)
-def kthSmallest(root, k):
-    stack = []
-    node = root
-    while node:     
-        stack.append(node)
-        node = node.left
-        
-    for i in range(k):
-        node = stack.pop()
-        res = node.val
-        
-        node = node.right
-        while node:        
-            stack.append(node)
-            node = node.left
-            
-    return res
-
-
-# 270. Closest BST Value - binary search O(logn)
-def closestValue(root, target):
-    p1, p2 = None, None
-  
-    node = root
-    while node:
-        if target > node.val:
-            p1 = node
-            node = node.right
-        elif target < node.val:
-            p2 = node
-            node = node.left
-        else:
-            return node.val
- 
-    return min(p1.val, p2.val, key=lambda x: abs(x - target))
-
-
-# 272. K Closest BST Values 
-# recursion O(n)
-def kClosestValues(root, target, k):
-    res = deque()
-
-    def rec(root):        # traverse
-        if root is None:
-            return
-        rec(root.left)
-        if len(res) == k:
-            if not abs(target - root.val) < abs(target - res[0]):
-                return
-            res.popleft()
-        res.append(root.val)
-        rec(root.right)
-
-    rec(root)
-    return list(res)
-  
- 
-# binary search + iterator O(logn + k)
-"""
-                   5 <            BST = [2, 3, 4, 5, 6, 7, 8]
-                 /   \            target = 6.1
-                3     7 <
-               / \   / \
-              2   4 6   8
-                    ^
-          
-          
-- closest itreator                   
-stack = path = [5, 7, 6]
-lower               upper                result
-[5, 7, 6]           [5, 7]               6
-[5]                                      7
-                    [5, 7, 8]            5
-[5, 3, 4]                                8
-                    []                   4 
-[5, 3]                                   3
-[5, 3, 2]                                2
-[]
-- smallest iterator
-getSuccessor              pop
-[5, 3, 2]                 2
-                          3
-[5, 4]                    4
-                          5
-[7, 6]                    6
-                          7
-[8]                       8
-[]
-"""
-class Solution:
-    
-    def closestKValues(self, root, target, k):
-        if root is None or k == 0:
-            return []
-        
-        # binary search
-        path = self.pathToTarget(root, target) # path = [5, 7, 6] 
-        stack = list(path) 
-        if stack[-1].val < target:
-            self.getSuccessor(stack)           
-            upperStack = stack          # upper [5, 7]
-            lowerStack = path           # lower [5, 7, 6]
-        else:
-            self.getPredecessor(stack)
-            lowerStack = stack
-            upperStack = path
-        
-        # merge
-        result = []
-        for i in range(k):
-            if self.isPredecessorCloser(lowerStack, upperStack, target):
-                result.append(lowerStack[-1].val)
-                self.getPredecessor(lowerStack) 
-            else:
-                result.append(upperStack[-1].val)
-                self.getSuccessor(upperStack)
-                
-        return result
-        
-    def pathToTarget(self, root, target):   # O(logn)
-        stack = []
-        while root:
-            stack.append(root)
-            if target < root.val:
-                root = root.left
-            else:
-                root = root.right
-                
-        return stack
-        
-    def getSuccessor(self, stack):     # successor
-        if stack[-1].right:
-            node = stack[-1].right
-            while node:
-                stack.append(node)
-                node = node.left
-        else:
-            node = stack.pop()
-            while stack and stack[-1].right == node:
-                node = stack.pop()
-                
-    def getPredecessor(self, stack):   # predecessor
-        if stack[-1].left:
-            node = stack[-1].left
-            while node:
-                stack.append(node)
-                node = node.right
-        else:
-            node = stack.pop()
-            while stack and stack[-1].left == node:
-                node = stack.pop()
-                
-    def isPredecessorCloser(self, lowerStack, upperStack, target):
-        if not lowerStack:
-            return False
-            
-        if not upperStack:
-            return True
-            
-        return target - lowerStack[-1].val <= upperStack[-1].val - target
-
-    
-    
-    
-    
-    
-    
-    
     
