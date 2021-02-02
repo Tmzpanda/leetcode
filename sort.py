8023b572603f1cea07fe58d81d9fe3cfe3dba1fb
Tim Zhang <tmzpanda@gmail.com>
2021-02-01 19:48:21 -0500

    Update sort.py

--- sort.py
+++ sort.py
@@ -92,33 +92,10 @@ def sortColors(nums):
 
             
 # 215. Kth Largest Element in an Array
-class Solution:
-    def findKthLargest(self, nums: List[int], k: int):      # k -> n-k+1 -> n-k
-        n = len(nums)
-        return self.quickSelect(nums, 0, n - 1, n - k)
-        
-    
-    def quickSelect(self, nums, start, end, k):     # wrong
-        l, r = start, end
-        pivot = (l + r) // 2
+# quick select O(n)
 
-        # partition
-        while l <= r:
-            while l <= r and nums[l] < nums[pivot]:
-                l += 1
-            while l <= r and nums[r] > nums[pivot]:
-                r -= 1
-            if l <= r:
-                nums[l], nums[r] = nums[r], nums[l]
-                l += 1
-                r -= 1
 
-        if k <= r:
-            return self.quickSelect(nums, start, r, k)
-        if k >= l:
-            return self.quickSelect(nums, l, end, k)
 
-        return nums[k]
 
 
 
