0db9e662dc69d7e1679f0ebf4e15201cc47a839c
Tim Zhang <tmzpanda@gmail.com>
2021-02-09 18:57:43 -0500

    Update tree.py

--- tree.py
+++ tree.py
@@ -5,7 +5,7 @@
 # 236. LCA in Binary Tree - d&q O(n) O(n)(recursion stack)
 # 578. LCA in Binary Tree - may not exist - d&q O(n) O(n)
 # 112. Binary Tree Path Sum - if exists
-# 437.          Subpath Sum - number of solutions - psum
+# 437. Binary Tree Subpath Sum - number of solutions - psum
             
 
 # traverse 
@@ -142,7 +142,61 @@ class Solution:
             
         return self.dfs(node.left, target - node.val) or self.dfs(node.right, target - node.val)
       
-      
+
+# 437. Binary Tree Subpath Sum - number of solutions - psum
+# bottom-up
+class Solution:
+    def pathSum(self, root, target):
+
+        self.count = 0      
+        self.dfs(root, 0, {0: 1}, target)
+        
+        return self.count
+
+
+    def dfs(self, root, psum, psum_to_freq, target):
+        if not root:
+            return 
+        
+        psum += root.val
+        if psum - target in psum_to_freq:
+            self.count += psum_to_freq[psum - target]
+    
+    
+        psum_to_freq[psum] = psum_to_freq.get(psum, 0) + 1
+
+       	self.dfs(root.left, psum, psum_to_freq, target)
+       	self.dfs(root.right, psum, psum_to_freq, target)
+
+       	psum_to_freq[psum] -= 1 
+
+# top-down
+class Solution(object):
+    def pathSum(self, root, target):
+
+        psum_to_freq = {0:1}
+        return self.dfs(root, 0, target, psum_to_freq)
+
+
+    def dfs(self, root, psum, target, psum_to_freq):
+        if not root:
+            return 0
+
+        res = 0
+        psum += root.val
+        if psum - target in psum_to_freq:
+            res += psum_to_freq[psum - target]
+        
+        psum_to_freq[psum] = psum_to_freq.get(psum, 0) + 1
+
+        res += self.dfs(root.left, psum, target, psum_to_freq)
+        res += self.dfs(root.right, psum, target, psum_to_freq)
+
+        psum_to_freq[psum] -= 1
+
+        return res
+
+
 #****************************************** traverse ***************************************************    
 # 257. Binary Tree Paths - all solutions
 class Solution:
