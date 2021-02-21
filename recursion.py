4c0be49e4473877876b368727718528477a70904
Tim Zhang <tmzpanda@gmail.com>
2021-02-21 02:29:39 -0500

    Update recursion.py

--- recursion.py
+++ recursion.py
@@ -259,55 +259,5 @@ class Solution:
 
       
       
-# ********************************************* return **********************************************************    
-# recursion top-down with return
-def partition(nums, start, end, k):
 
-    l, r = start, end
-    pivot = nums[(start + end) // 2]
-
-    while l <= r:
-        while l <= r and nums[l] < pivot:
-            l += 1
-        while l <= r and nums[r] > pivot:
-            r -= 1
-        if l <= r:
-            nums[l], nums[r] = nums[r], nums[l]
-            l += 1
-            r -= 1
-
-    if k <= r:
-        return partition(nums, start, r, k)
-    if k >= l:
-        return partition(nums, l, end, k)
-        
-    return nums[k]
-
-
-# recursion top-down
-def kthSmallest(nums, k):
-    partition(nums, 0, len(nums - 1), k - 1)
-
-    return nums[k - 1]
-
-
-def partition(nums, start, end, k):
-
-    l, r = start, end
-    pivot = nums[(start + end) // 2]
-
-    while l <= r:
-        while l <= r and nums[l] < pivot:
-            l += 1
-        while l <= r and nums[r] > pivot:
-            r -= 1
-        if l <= r:
-            nums[l], nums[r] = nums[r], nums[l]
-            l += 1
-            r -= 1
-
-    if k <= r:
-        partition(nums, start, r, k)
-    if k >= l:
-        partition(nums, l, end, k)
 
