0ef90af4f4b0b1e70ed67e6c3fccf7787ed6a337
Tim Zhang <tmzpanda@gmail.com>
2021-02-27 17:01:16 -0500

    Update sort.py

--- sort.py
+++ sort.py
@@ -17,7 +17,7 @@
 # 349. Intersection of Two Arrays - two pointers O(m + n)
                                   - binary search O(m * logn)
 # 4. Median (Kth) of Two Sorted Arrays - merge O(m + n)
-                                       - binary search O(m + n)         X
+                                       - binary search O(m + n)         
 # 912. Merge Sort O(nlogn)
 # 23. Merge k Sorted Lists - O(Nlogn), where n is # of LinkedLists, N is # of nodes
 
@@ -144,7 +144,7 @@ def topKFrequent(nums, k):
     return res
   
   
-# heap O(nlogn)
+# heap O(nlogk)
 from heapq import heappush, heappop
 def topKFrequent(nums, k):
         
