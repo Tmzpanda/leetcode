6050a694f5975e33cefdbf18dc4a11abb06dfcb8
Tim Zhang <tmzpanda@gmail.com>
2021-02-28 14:26:25 -0500

    Update topics.py

--- array/topics.py
+++ array/topics.py
@@ -18,6 +18,7 @@
 139. Word Break - if possible - dp O(n^2)
                 - number of solutions - dp O(n^2)
 140.            - all solutions - dfs memoization 
+91. Decode Ways - number of solutions - dp O(n)
                 
 127. Word Ladder - shortest path - bfs O(n * m^2)
                                  - bidirectional bfs
@@ -281,7 +282,32 @@ class Solution:
                 combination.pop()
                 
                 
-                
+# 91. Decode Ways - number of solutions - dp O(n)
+class Solution:
+    def numDecodings(self, s):
+        if s.startswith('0'):
+            return 0
+        
+        n = len(s)
+        dp = [0 for _ in range(n + 1)]
+        dp[0] = 1
+        dp[1] = 1
+        
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 1] * self.isValid(s[i - 1]) + dp[i - 2] * self.isValid(s[i - 2: i])
+            
+        return dp[n]
+    
+    def isValid(self, string):
+        n = len(string)
+        num = int(string)
+        if n == 1 and 1<= num <= 9:
+            return 1
+        if n == 2 and 10 <= num <= 26:
+            return 1
+        return 0                 
+               
+        
 # 79. Word Search - if exists - dfs O(mn * 3^length)
 DIRECTIONS = [(0, -1), (-1, 0), (0, 1), (1, 0)]
 class Solution:
