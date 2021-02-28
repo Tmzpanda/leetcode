aa7fd6ba86381cb26398745d969f4dd027767458
Tim Zhang <tmzpanda@gmail.com>
2021-02-28 10:29:45 -0500

    Update binary_search.py

--- array/binary_search.py
+++ array/binary_search.py
@@ -84,24 +84,42 @@ def searchBigSortedArray(reader, target):
       
       
 # 35. Search Insert Position
+def searchInsert(nums, target):
+    l, r = 0, len(nums) - 1
 
+    while l + 1 < r:
+        mid = (l + r) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            l = mid
+        else:
+            r = mid
+
+    if nums[l] >= target:     # special case
+        return l
+    if nums[r] >= target:
+        return r
+    return len(nums)
+  
+# K closest  
 def findUpperClosest(self, A, target):
     l, r = 0, len(A) - 1
     while l + 1 < r:
         mid = (l + r) // 2
         if target == A[mid]:
             return mid
-        elif target < A[mid]:
-            r = mid
-        else:
+        elif A[mid] < target:
             l = mid
-            
+        else:
+            r = mid
+                 
     return r
 
 
 # 354. Russian Doll Envelopes - binary search O(nlogn)
 """
-(2, 3), (5, 3), (6, 7), (6, 4)
+(2, 3), (5, 3), (6, 4), (6, 7)
 
 3, 3, 7, 4
 
