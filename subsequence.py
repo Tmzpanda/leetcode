1c2f107fbf0965d98f28535b7369a9978de53d92
Tim Zhang <tmzpanda@gmail.com>
2021-02-27 22:49:58 -0500

    Update subsequence.py

--- subsequence.py
+++ subsequence.py
@@ -501,3 +501,52 @@ def longestPalindromeSubseq(s):
     return dp[0][n - 1]
   
 
+  
+# 131. Palindrome Partitioning - all solutions - backtrack
+#                                              - memoization
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        res = []
+        self.dfs(s, [], res)
+        return res
+    
+    def dfs(self, s, substrings, res):
+        if not s:
+            res.append(substrings[:])
+            return 
+        
+        for i in range(len(s)):
+            prefix = s[: i + 1]
+            if self.isPalindrome(prefix):
+                substrings.append(prefix)
+                self.dfs(s[i + 1:], substrings, res)
+                substrings.pop()
+            
+    def isPalindrome(self, s):
+        return s == s[::-1]
+
+
+
+# 132. Palindrome Partitioning - minimum cut - dp O(n^2)
+import sys
+class Solution:
+    def minCut(self, s):
+        return self.dfs(s, {})
+        
+    def dfs(self, s, memo):
+        n = len(s) 
+        dp = [sys.maxsize] * n
+        
+        for i in range(n):
+            if self.isPalindrome(s[:i + 1]):
+                dp[i] = 0
+            else:
+                for j in range(i):
+                    if self.isPalindrome(s[j + 1: i + 1]):   # s[j + 1 to i]
+                        dp[i] = min(dp[i], dp[j] + 1)
+                    
+        return dp[n - 1]
+    
+    def isPalindrome(self, s):
+        return s == s[::-1]
+
