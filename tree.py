d15ddec5f1975b42f86fb2f405c4157e741f56c8
Tim Zhang <tmzpanda@gmail.com>
2021-03-01 01:03:03 -0500

    Update tree.py

--- tree.py
+++ tree.py
@@ -1,31 +1,26 @@
 """
+top-down(d&q) VS bottom-up(backtrack, traverse)
 
-top-down(d&q) VS bottom-up(traverse, backtrack)
 
-# d&q
-# 226. Invert Binary Tree - O(n)
-# 235. LCA in BST - iteration O(logn)
+# 226. Invert Binary Tree - d&q O(n)
+# 235. LCA in BST - binary search O(logn)
 # 236. LCA in Binary Tree - d&q O(n) O(n)(recursion stack)
 # 578. LCA in Binary Tree - may not exist - d&q O(n) O(n)
-# 112. Binary Tree Path Sum - if exists - d&q
 
+
+# 112. Binary Tree Path Sum - if exists - d&q
 # 257. Binary Tree Paths - all solutions - backtrack
 # 113. Binary Tree Path Sum - all solutions - backtrack
 # 437. Binary Tree Subpath Sum - number of solutions - psum
      
      
-
-# traverse 
 # 272. K Closest BST Values - inorder traverse O(n)
                             - iterator O(logn + k)                           
 # 102. Binary Tree Level Order Traversal - bfs
 # 297. Serialize and Deserialize Binary Tree - dfs
                                              - bfs
                                              
-
 """
-
-
 #***************************************** d&q ***************************************************
 # 226. Invert Binary Tree
 def invertTree(root):
@@ -129,7 +124,8 @@ class Solution:
         return a, b, None 
 
 
-            
+
+#***************************************** backtrack ***************************************************            
 # 112. Binary Tree Path Sum - if exists
 class Solution:
     def hasPathSum(self, root: TreeNode, target: int) -> bool:
