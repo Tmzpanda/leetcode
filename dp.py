c10619c72e0036721b6eb92ac60001ca55e52ba1
Tim Zhang <tmzpanda@gmail.com>
2021-02-28 20:13:58 -0500

    Update dp.py

--- dp.py
+++ dp.py
@@ -377,7 +377,7 @@ class Solution:
             for s in range(1, amount + 1):
                 if coins[i - 1] <= s:
                     dp[i][s] = dp[i][s - coins[i -1]] + dp[i - 1][s]    # repeated use
-#                     dp[i][s] = dp[i - 1][s - coins[i -1]] + dp[i - 1][s]    # used once
+#                   dp[i][s] = dp[i - 1][s - coins[i -1]] + dp[i - 1][s]    # used once
                 else:
                     dp[i][s] = dp[i - 1][s] 
                     
@@ -385,13 +385,37 @@ class Solution:
 
       
 # 494. Assign symbols to Target Sum - number of solutions - dp O(n*S)
+"""
+nums = [1, 2, 1] -> s = 4
+S = 0
 
+                    s
+        -4 -3 -2 -1 0 1 2 3 4 
+ nums 0  0  0  0  0 0 0 0 0 0
+      1
+      2           x   x
+      1             x = dp[i - 1][j - nums[i - 1]] + dp[i - 1][j + nums[i - 1]]
+                    
 
+"""
+def findTargetSumWays(nums, S):
+    if sum(nums) < S:
+        return 0
+    
+    n = len(nums)
+    s = sum(nums)
+    dp = [[0] * (2*s + 1) for _ in range(n + 1)]
+    dp[0][s] = 1
 
-
-
-
-      
+    for i in range(1, n + 1):
+        for j in range(2*s + 1):
+            if j - nums[i - 1] >= 0:
+                dp[i][j] += dp[i - 1][j - nums[i - 1]]
+            if j + nums[i - 1] <= 2*s:
+                dp[i][j] += dp[i - 1][j + nums[i - 1]]
+    
+    return dp[-1][s + S]
+ 
 
 # ************************************** O(S*n) ***************************************************
 # 377. Combination Sum - number of solutions - dp O(S*n)
@@ -468,7 +492,7 @@ def maximalSquare(matrix):
                 dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
                 
     res = max(max(row) for row in dp)
-#     res = max(map(lambda x: max(x), a))
+#   res = max(map(lambda x: max(x), a))
     return res ** 2 
   
   
