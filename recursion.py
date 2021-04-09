8bcd262e22989e97129dc84f37f7f29aac52c6f8
Tim Zhang <tmzpanda@gmail.com>
2021-04-09 00:20:23 -0400

    Update recursion.py

--- recursion.py
+++ recursion.py
@@ -53,34 +53,33 @@ class Solution:
 
 
 # 583. Delete Distance 
-class Solution:
-    def minDistance(self, word1: str, word2: str) -> int:
-        
-        def dfs(word1, word2, memo):
-            if (word1, word2) in memo:
-                return memo[(word1, word2)]
-            
-            if word1 == word2:
-                return 0
-            
-            if word1 == "":
-                return len(word2)
-        
-            if word2 == "":
-                return len(word1)
-            
-            
-            if word1[0] == word2[0]:
-                res = dfs(word1[1:], word2[1:], memo)
-                
-            else:
-                res = min(dfs(word1, word2[1:], memo) + 1,
-                          dfs(word1[1:], word2, memo) + 1
-                         )
-                
-            memo[(word1, word2)] = res
-            return res
-            
-        return dfs(word1, word2, {})
-            
-        
+def minDistance(word1: str, word2: str) -> int:
+
+    def dfs(word1, word2, memo):
+        if (word1, word2) in memo:
+            return memo[(word1, word2)]
+
+        if word1 == word2:
+            return 0
+
+        if word1 == "":
+            return len(word2)
+
+        if word2 == "":
+            return len(word1)
+
+
+        if word1[0] == word2[0]:
+            res = dfs(word1[1:], word2[1:], memo)
+
+        else:
+            res = min(dfs(word1, word2[1:], memo) + 1,
+                      dfs(word1[1:], word2, memo) + 1
+                     )
+
+        memo[(word1, word2)] = res
+        return res
+
+    return dfs(word1, word2, {})
+
+
