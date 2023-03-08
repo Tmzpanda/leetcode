3523132bde1dc95ab3bf1c8c03e89f72522b9ad4
Tim Zhang <tmzpanda@gmail.com>
2023-03-08 17:18:05 -0500

    Create 287. Find the Duplicate Number.py

---
+++ 287. Find the Duplicate Number.py
@@ -0,0 +1,12 @@
+# 287. Find the Duplicate Number
+
+def findDuplicates(nums):
+    res = []
+    
+    for num in nums:
+        i = abs(num) - 1
+        if nums[i] < 0:
+            res.append(abs(num))
+        nums[i] = - nums[i]   # mark the corresponding entries as negative
+        
+    return res
