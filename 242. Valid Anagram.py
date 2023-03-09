7e7bfa62eb45d64f0fe56fb46afdd0e119cd912f
Tim Zhang <tmzpanda@gmail.com>
2023-03-09 14:01:40 -0500

    Update 242. Valid Anagram.py

--- 242. Valid Anagram.py
+++ 242. Valid Anagram.py
@@ -3,16 +3,18 @@
 
 # hashmap
 def isAnagram(s: str, t: str) -> bool:
-    freq_dict = {}
+    char_freq = {}
     for char in s:
-      
+        char_freq[char] = char_freq.get(char, 0) + 1
 
+    for char in t:
+        char_freq[char] = char_freq.get(char, 0) - 1
 
-
-
-# array mark
-
-
+    return all(v == 0 for v in char_freq.values())
+     
 
 
 # sort 
+def isAnagram(self, s: str, t: str) -> bool:
+    
+    return sorted(s) == sorted(t)
