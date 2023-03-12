25c4bb55165e4223744b2ee15abf8c7d1a5c821d
Tim Zhang <tmzpanda@gmail.com>
2023-03-12 15:59:34 -0400

    Update 322. Coin Change.py

--- 07-Recursion & DP/322. Coin Change.py
+++ 07-Recursion & DP/322. Coin Change.py
@@ -1,13 +1,15 @@
-# 322. Coin Change - fewest coins - dp O(S)
+# 322. Coin Change 
+
+# dp O(S)
 def coinChange(coins, amount):
     
     dp = [sys.maxsize for i in range(amount + 1)]
     dp[0] = 0
     
     for s in range(1, amount + 1):
-        for coin in coins:
+        for coin in coins:      # infinite number of each kind of coin
             if coin <= s:
-                dp[s] = min(dp[s], dp[s - coin] + 1)       
+                dp[s] = min(dp[s], dp[s - coin] + 1)      
     
     return dp[amount] if dp[amount] != sys.maxsize else -1
 
