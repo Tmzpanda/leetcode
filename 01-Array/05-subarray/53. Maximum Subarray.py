0d352a026028998c03a3e17d72e5ede1edde8b39
Tim Zhang <tmzpanda@gmail.com>
2023-03-12 15:17:19 -0400

    Update 53. Maximum Subarray.py

--- 01-Array/05-subarray/53. Maximum Subarray.py
+++ 01-Array/05-subarray/53. Maximum Subarray.py
@@ -1,11 +1,14 @@
-# 53. Maximum Subarray - greedy O(n)
+# 53. Maximum Subarray 
+
+# greedy O(n)
 def maxSubArray(nums):
-    min_sum, max_sum = 0, -sys.maxsize
+    min_sum = 0
+    max_sum = -sys.maxsize
     prefix_sum = 0
     
     for num in nums:
         prefix_sum += num
-        max_sum = max(max_sum, prefix_sum - min_sum)
-        min_sum = min(min_sum, prefix_sum)
+        max_sum = max(prefix_sum - min_sum, max_sum)
+        min_sum = min(prefix_sum, min_sum)
         
     return max_sum
