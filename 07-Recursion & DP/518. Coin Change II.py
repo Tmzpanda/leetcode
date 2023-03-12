e04cb508d6cc20e8424348da8ea0fef24ed3b2be
Tim Zhang <tmzpanda@gmail.com>
2023-03-12 19:22:03 -0400

    Update and rename 518. Coin Change.py to 518. Coin Change II.py

---
+++ 07-Recursion & DP/518. Coin Change II.py
@@ -0,0 +1,16 @@
+
+# 518. Coin Change II - number of solutions
+
+# dp O(n*S)
+def coinChange(amount: int, coins: List[int]) -> int:
+    
+    n = len(coins)
+    dp = [0 for _ in range(amount + 1)]
+    dp[0] = 1
+
+    for i in range(n):
+        for s in range(coins[i], amount + 1):
+            dp[s] += dp[s - coins[i]]
+
+    return dp[amount]
+
