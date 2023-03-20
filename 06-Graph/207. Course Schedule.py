c60477a9776d67ff0d5e93fdcfa0d4d3f5ecd9b5
Tim Zhang <tmzpanda@gmail.com>
2023-03-20 13:04:23 -0400

    Update 207. Course Schedule.py

--- 06-Graph/207. Course Schedule.py
+++ 06-Graph/207. Course Schedule.py
@@ -1,23 +1,26 @@
 # 207. Course Schedule - one possible solution - bfs
-from collections import deque
 
 def findOrder(numCourses, prerequisites):
-    out_edges = [[] for _ in range(numCourses)]
+    
+    # build graph
+    graph = [[] for _ in range(numCourses)]
     in_degrees = [0 for _ in range(numCourses)]
     for t, f in prerequisites:
-        out_edges[f].append(t)
+        graph[f].append(t)
         in_degrees[t] += 1
 
-    queue = deque([node for node in range(numCourses) if in_degrees[node] == 0])    # indegree
+    # topological sort
+    queue = deque([node for node in range(numCourses) if in_degrees[node] == 0])    
     order = []
     while queue:
-        node = queue.popleft()      # not necessarily popoleft in topsort
+        node = queue.pop()      # not necessarily popoleft in topsort
         order.append(node)
-        for next_node in out_edges[node]:
+        for next_node in graph[node]:
             in_degrees[next_node] -= 1
             if in_degrees[next_node] == 0:
                 queue.append(next_node)
 
     if len(order) == numCourses:
         return order
+
     return []
