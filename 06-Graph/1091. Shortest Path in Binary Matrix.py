54524ca598356702848e3b2e674673550d410c8b
Tim Zhang <tmzpanda@gmail.com>
2023-03-20 21:04:00 -0400

    Update 1091. Shortest Path in Binary Matrix.py

--- 06-Graph/1091. Shortest Path in Binary Matrix.py
+++ 06-Graph/1091. Shortest Path in Binary Matrix.py
@@ -4,17 +4,19 @@ def shortestPathBinaryMatrix(grid: List[List[int]]) -> int:
     n = len(grid)
     if grid[0][0] == 1 or grid[n-1][n-1] == 1:
         return -1
-    
+
     directions = [(-1, 0), (-1, -1), (-1, 1), (0, 1), (0, -1), (1, 0), (1, 1), (1, -1)]
     queue = deque([(0, 0, 1)])
+    visited = set([(0, 0)])
     while queue:
-        i, j, dist = queue.popleft()
+        i, j, distance = queue.popleft()
         if i == n-1 and j == n-1:
-            return dist
-        for d in directions:
-            x, y = i + d[0], j + d[1]
-            if 0 <= x < n and 0 <= y < n and grid[x][y] == 0:
-                grid[x][y] = 1 
-                queue.append((x, y, dist + 1))
+            return distance
+        
+        for delta in directions:
+            x, y = i + delta[0], j + delta[1]
+            if 0 <= x < n and 0 <= y < n and grid[x][y] == 0 and (x, y) not in visited:
+                queue.append((x, y, distance + 1))
+                visited.add((x, y))
 
     return -1
