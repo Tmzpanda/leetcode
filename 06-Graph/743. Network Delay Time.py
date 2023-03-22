bd584d833033d9dbfb4a6a0715f50d61b3c79907
Tim Zhang <tmzpanda@gmail.com>
2023-03-22 16:26:21 -0400

    Update 743. Network Delay Time.py

--- 06-Graph/743. Network Delay Time.py
+++ 06-Graph/743. Network Delay Time.py
@@ -10,16 +10,16 @@ def networkDelayTime(times: List[List[int]], n: int, k: int) -> int:
     # dijkstra
     min_heap = [(0, k)] # (distance_to_origin, current_node)
     visited = set()
-    total_time = 0
+    res = 0
     while min_heap:
         current_weight, current_node = heappop(min_heap)
         if current_node not in visited:     # 
             visited.add(current_node)
-            total_time = max(total_time, current_weight)
+            res = max(res, current_weight)
 
         for next_node, next_weight in graph[current_node]:
             if next_node not in visited:
                 heappush(min_heap, (current_weight + next_weight, next_node))
 
-    return total_time if len(visited) == n else -1
+    return res if len(visited) == n else -1
 
