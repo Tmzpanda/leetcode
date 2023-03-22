24aef4ee373d0e477d3eb6031b14bc58e68f9696
Tim Zhang <tmzpanda@gmail.com>
2023-03-22 15:40:21 -0400

    Update 79. Word Search.py

--- 06-Graph/79. Word Search.py
+++ 06-Graph/79. Word Search.py
@@ -1,39 +1,27 @@
-# 79. Word Search - if exists - dfs O(mn * 3^length)
-DIRECTIONS = [(0, -1), (-1, 0), (0, 1), (1, 0)]
-class Solution:
-    def exist(self, board, word):
-        m, n = len(board), len(board[0])
-        for i in range(m):
-            for j in range(n):
-                if board[i][j] == word[0]:
-                    if self.dfs(board, i, j, {(i, j)}, word, 1):
-                        return True
-        return False
+# 79. Word Search - if exists
+
+# dfs O(mn * 3^L)
+def exist(self, board, word):
+    m, n = len(board), len(board[0])
+    for i in range(m):
+        for j in range(n):
+            if board[i][j] == word[0]:
+                if self.dfs(board, i, j, {(i, j)}, word, 1):
+                    return True
+    return False
     
-    
-    def dfs(self, board, x, y, visited, word, index):
-        if index == len(word):
-            return True
-        
-        for delta_x, delta_y in DIRECTIONS:
-            x_next, y_next = x + delta_x, y + delta_y
-            if not self.isValid(board, x_next, y_next, visited, word, index):
-                continue
-            
-            visited.add((x_next, y_next))
-            if self.dfs(board, x_next, y_next, visited, word, index + 1):
+
+def dfs(board, i, j, visited, word, index):
+    if index == len(word):
+        return True
+
+    for delta in DIRECTIONS:
+        x, y = i + delta[0], j + delta[1]
+        if 0 <= x < len(board) and 0 <= y < len(board[0]) and (x, y) not in visited and board[x][y] == word[index]:
+            visited.add((x, y))
+            if self.dfs(board, x, y, visited, word, index + 1):
                 return True
-            visited.discard((x_next, y_next))
-        
-        return False
-            
-            
-    def isValid(self, grid, i, j, visited, word, index):
-        n, m = len(grid), len(grid[0])
-        if not (0 <= i < n and 0 <= j < m):
-            return False
-        if (i, j) in visited:
-            return False
-        
-        return grid[i][j] == word[index]
-      
+            visited.discard((x, y))
+
+    return False
+ 
