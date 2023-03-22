970a7f3740b8dd5807684e2bfb8da599202d499a
Tim Zhang <tmzpanda@gmail.com>
2023-03-22 16:07:46 -0400

    Update 257. Binary Tree Paths.py

--- 05-Tree/257. Binary Tree Paths.py
+++ 05-Tree/257. Binary Tree Paths.py
@@ -1,10 +1,9 @@
+# 257. Binary Tree Paths
 
-# all paths
-# https://leetcode.com/problems/binary-tree-paths/description/
 def binaryTreePaths(root):
 
     def dfs(root, path, res):
-        if root.left is None and root.right is None:
+        if not root.left and not root.right:
             res.append(list(path))
             return
 
@@ -12,38 +11,9 @@ def binaryTreePaths(root):
             if node:
                 path.append(str(node.val))
                 dfs(node, path, res)
-                path.pop()
+                path.pop()       # backtrack
 
-    path = [str(root.val)]
     res = []
-    dfs(root, path, res)
+    dfs(root, [str(root.val)], res)
 
     return ['->'.join(path) for path in res]
-
-
-
-# 257. Binary Tree Paths - all solutions
-class Solution:
-    def binaryTreePaths(root):
-        if root is None:
-            return []
-        
-        res = []
-        self.dfs(root, [str(root.val)], res)
-        return res
-    
-    def dfs(self, node, path, res):
-        if not node.left and not node.right:
-            res.append('->'.join(path))
-            return
-        
-        if node.left:
-            path.append(str(node.left.val))
-            self.dfs(node.left, path, res)      # backtrack 
-            path.pop() 
-        
-        if node.right:
-            path.append(str(node.right.val))
-            self.dfs(node.right, path, res)
-            path.pop()   
-            
