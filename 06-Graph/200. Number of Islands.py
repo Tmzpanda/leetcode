28a2381e07f44f6867e6110d5e6eb815022bca03
Tim Zhang <tmzpanda@gmail.com>
2023-03-22 15:49:33 -0400

    Update 200. Number of Islands.py

--- 06-Graph/200. Number of Islands.py
+++ 06-Graph/200. Number of Islands.py
@@ -11,6 +11,7 @@ def numIslands(grid):
         for j in range(len(grid[0])):
             if grid[i][j] == "1" and (i, j) not in visited:
                 res += 1
+                visited.add((i, j))
                 bfs(grid, i, j, visited)        # bfs or dfs
 
     return res      
@@ -21,7 +22,6 @@ def bfs(grid, i, j, visited):
     m, n = len(grid), len(grid[0])
     directions = [(-1, 0), (0, 1), (0, -1), (1, 0)]
     queue = deque([(i, j)])
-    visited.add((i, j))
     while queue:
         i, j = queue.popleft()
         for delta in directions:
@@ -30,13 +30,11 @@ def bfs(grid, i, j, visited):
                 visited.add((x, y))
                 queue.append((x, y))
                 
-
-      
+     
 # dfs
 def dfs(grid, i, j, visited):
     m, n = len(grid), len(grid[0])
     directions = [(-1, 0), (0, 1), (0, -1), (1, 0)]
-    visited.add((i, j))
     for delta in directions:
         x, y = i + delta[0], j + delta[1]
         if 0 <= x < m and 0 <= y < n and (x, y) not in visited and grid[x][y] == "1":
