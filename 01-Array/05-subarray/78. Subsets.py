ce31e89ab4ff52b5c81c899d322520e528aecb98
Tim Zhang <tmzpanda@gmail.com>
2023-03-26 14:12:46 -0400

    Update 78. Subsets.py

--- 01-Array/05-subarray/78. Subsets.py
+++ 01-Array/05-subarray/78. Subsets.py
@@ -1,1 +1,18 @@
+# 78. Subsets
+
+def subsets(nums: List[int]) -> List[List[int]]:
+    nums = sorted(nums)
+    n = len(nums)
+
+    def dfs(index, subset):
+        res.append(list(subset))
+        for i in range(index, n):
+            subset.append(nums[i])
+            dfs(i + 1, subset)
+            subset.pop()
+
+    res = []
+    dfs(0, [])
+
+    return res
 
