afa0e40e4ef233663104055d4874dec718741dfc
Tim Zhang <tmzpanda@gmail.com>
2023-03-26 14:51:12 -0400

    Update 46. Permutation.py

--- 01-Array/05-subarray/46. Permutation.py
+++ 01-Array/05-subarray/46. Permutation.py
@@ -5,7 +5,7 @@ def permute(nums: List[int]) -> List[List[int]]:
     def dfs(index):
         if index == n:
             res.append(list(nums))
-            return 
+            
         for i in range(index, n):
             nums[index], nums[i] = nums[i], nums[index]
             dfs(index + 1)
