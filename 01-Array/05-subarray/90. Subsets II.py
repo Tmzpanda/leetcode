26a071fe1eb39375454f36074dc15dead9df0ee4
Tim Zhang <tmzpanda@gmail.com>
2023-03-26 14:15:44 -0400

    Create 90. Subsets II.py

---
+++ 01-Array/05-subarray/90. Subsets II.py
@@ -0,0 +1,21 @@
+# 90. Subsets II - duplicate exists
+
+def subsets(nums: List[int]) -> List[List[int]]:
+    nums = sorted(nums)
+    n = len(nums)
+
+    def dfs(index, subset):
+        res.append(list(subset))
+        for i in range(index, n):
+            if i != 0 and nums[i] == nums[i - 1] and i != index:    # deduplicate
+                continue
+            subset.append(nums[i])
+            dfs(i + 1, subset)
+            subset.pop()
+
+    res = []
+    dfs(0, [])
+
+    return res
+  
+  
