3ba98552a82c07bf86671daf00cbc6f43a5e1cc9
Tim Zhang <tmzpanda@gmail.com>
2023-03-26 14:42:53 -0400

    Update 47. Permutation.py

--- 01-Array/05-subarray/47. Permutation.py
+++ 01-Array/05-subarray/47. Permutation.py
@@ -1,21 +1,17 @@
 # 47. Permutation - O(n!)
-class Solution:
-    def permutation(self, nums):
-        nums.sort()
-        used = [False] * len(nums)
-        result = []
-        self.dfs(nums, used, [], result)
-        return result
-        
-    def dfs(self, nums, used, path, result):
-        if len(path) == len(nums):
-            result.append(list(path)) 
+def permute(nums: List[int]) -> List[List[int]]:
+    n = len(nums)
+
+    def dfs(index):
+        if index == n:
+            res.append(list(nums))
             return 
-        
-        for i in range(len(nums)):
-            if used[i] or (i > 0 and nums[i] == nums[i - 1] and not used[i - 1]):       # deduplicate
-                continue
-                
-            used[i] = True
-            self.dfs(nums, used, path + [nums[i]], result)   
-            used[i] = False
+        for i in range(index, n):
+            nums[index], nums[i] = nums[i], nums[index]
+            dfs(index + 1)
+            nums[index], nums[i] = nums[i], nums[index]
+
+    res = []
+    dfs(0)
+
+    return res
