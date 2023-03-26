b5106e73a3a1ae8ad7c30d0be3cc2abcc1518b31
Tim Zhang <tmzpanda@gmail.com>
2023-03-26 18:23:46 -0400

    Update 589. N-ary Tree Preorder Traversal.py

--- 05-Tree/589. N-ary Tree Preorder Traversal.py
+++ 05-Tree/589. N-ary Tree Preorder Traversal.py
@@ -1,1 +1,15 @@
+# 589. N-ary Tree Preorder Traversal
 
+def preorder(self, root: Optional[TreeNode]) -> List[int]:
+    def dfs(root):
+        if node is None:
+            return
+        res.append(node.val)
+        for node in node.children:
+            dfs(node)
+
+    res = []
+    dfs(root)
+    return res
+  
+  
