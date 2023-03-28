bf119a8e2d6ef4a14d0969921fddf329ededacba
Tim Zhang <tmzpanda@gmail.com>
2023-03-28 12:22:13 -0400

    Update 912. Sort an Array (Merge Sort).py

--- 01-Array/03-sort/912. Sort an Array (Merge Sort).py
+++ 01-Array/03-sort/912. Sort an Array (Merge Sort).py
@@ -11,23 +11,24 @@ def mergeSort(nums: List[int]) -> List[int]:
     right = mergeSort(nums[mid:])
 
     # merge
+    merged = []     
     i, j, k = 0, 0, 0
     while i < len(left) and j < len(right):
         if left[i] < right[j]:
-            nums[k] = left[i]
+            merged[k] = left[i]
             i += 1
         else:
-            nums[k] = right[j]
+            merged[k] = right[j]
             j += 1
         k += 1
     
     while i < len(left):
-        nums[k] = left[i]
+        merged[k] = left[i]
         i += 1
         k += 1
     while j < len(right):
-        nums[k] = right[j]
+        merged[k] = right[j]
         j += 1
         k += 1
 
-    return nums
+    return merged       # pure function
