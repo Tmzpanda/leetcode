b98cff2eff558e402e1f1059ec01cfa9c820f1d2
Tim Zhang <tmzpanda@gmail.com>
2023-03-28 13:34:52 -0400

    Update 23. Merge k Sorted Lists.py

--- 02-LinkedList/23. Merge k Sorted Lists.py
+++ 02-LinkedList/23. Merge k Sorted Lists.py
@@ -1,28 +1,31 @@
 # 23. Merge k Sorted Lists 
 # O(nlogk)
 def mergeKLists(lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+    # edge
+    if not lists: 
+        return None
     # base
     if len(lists) == 1:
-            return lists[0]
-        
+        return lists[0]
+
     # divide and conquer
     mid = len(lists) // 2
-    left = mergeKLists(lists[:mid])
-    right = mergeKLists(lists[mid:])
-    
-    return merge(left, right)
+    left = self.mergeKLists(lists[:mid])
+    right = self.mergeKLists(lists[mid:])
+
+    return self.merge(left, right)
     
     
 def merge(l1: ListNode, l2: ListNode) -> ListNode:
     cur = dummy = ListNode(0)
     while l1 and l2:
-        if l1.val < l1.val:
+        if l1.val < l2.val:
             cur.next = l1
-            l1 = p1.next
+            l1 = l1.next
         else:
             cur.next = l2
             l2 = l2.next
         cur = cur.next
-        
-    cur.next = p1 or p2
+
+    cur.next = l1 or l2
     return dummy.next 
