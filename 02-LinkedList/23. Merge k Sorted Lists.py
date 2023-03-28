444454b3dcbd94fee4e44c486e865d655f7bb848
Tim Zhang <tmzpanda@gmail.com>
2023-03-28 13:23:01 -0400

    Update 23. Merge k Sorted Lists.py

--- 02-LinkedList/23. Merge k Sorted Lists.py
+++ 02-LinkedList/23. Merge k Sorted Lists.py
@@ -1,30 +1,29 @@
-
-# 23. Merge k Sorted Lists - O(Nlogn), where n is # of LinkedLists, N is # of nodes
-def mergeNLists(lists):
-    return mergeRange(lists, 0, len(lists) - 1)
-
-def mergeRange(lists, start, end):
-    if start == end:
-        return lists[start]
-
-    mid = (start + end) // 2
-    left = mergeRange(lists, start, mid)
-    right = mergeRange(lists, mid + 1, end)
+# 23. Merge k Sorted Lists 
+# O(nlogk)
+def mergeKLists(lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+    # base
+    if len(lists) == 1:
+            return lists[0]
+        
+    # divide and conquer
+    mid = len(lists) // 2
+    left = mergeKLists(lists[:mid])
+    right = mergeKLists(lists[mid:])
+    
     return merge(left, right)
     
-def merge(l1, l2):
-    temp = dummy = ListNode(0)
-    p1, p2 = l1, l2
-    while p1 and p2:
-        if p1.val <= p2.val:
-            temp.next = p1
-            p1 = p1.next
-        else:
-            temp.next = p2
-            p2 = p2.next
-        temp = temp.next
-        
-    temp.next = p1 or p2
     
+def merge(l1: ListNode, l2: ListNode) -> ListNode:
+    cur = dummy = ListNode(0)
+    while l1 and l2:
+        if l1.val < l1.val:
+            cur.next = l1
+            l1 = p1.next
+        else:
+            cur.next = l2
+            l2 = l2.next
+        cur = cur.next
+        
+    cur.next = p1 or p2
+   
     return dummy.next 
-
