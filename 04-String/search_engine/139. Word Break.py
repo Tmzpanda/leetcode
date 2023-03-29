3f5e23edff38e7059b1f841857eef88f38b18476
Tim Zhang <tmzpanda@gmail.com>
2023-03-29 13:12:01 -0400

    Update 139. Word Break.py

--- 04-String/search_engine/139. Word Break.py
+++ 04-String/search_engine/139. Word Break.py
@@ -1,58 +1,36 @@
-# 139. Word Break - dp O(n^2)
-def wordBreak(s, wordDict):
-    
-    def dfs(s, wordDict, memo):
+# 139. Word Break
+# memoization
+def wordBreak(s: str, wordDict: List[str]) -> bool:
+    memo = {}
+    def dfs(s):
+        # memo
         if s in memo:
             return memo[s]
-        
+        # base
         if not s:
-            return 1
-    
-        res = 0
+            return True
+        # main
         for i in range(len(s)):
-            if s[: i + 1] in wordDict:
-                res += dfs(s[i + 1:], wordDict, memo)
-                
-        memo[s] = res
-        return res
-    
-    return dfs(s, wordDict, {})
+            if s[:i+1] in wordDict and dfs(s[i+1:]) == True:
+                memo[s] = True
+        memo[s] = False
+
+        return memo[s]
+
+    return dfs(s)
 
 
 
-# 139. Word Break - if possible - dp O(n^2)
-#                 - number of solutions - dp O(n^2)
-def wordBreak(s, wordSet):
-       
+# 139. Word Break 
+# dp 
+def wordBreak(s: str, wordDict: List[str]) -> bool:
     n = len(s)
-    dp = [False for _ in range(n + 1)] 
+    dp = [False] * (n+1)
     dp[0] = True
-   
-    for i in range(n + 1):
-       for j in range(i + 1, n + 1):
-           if dp[i] and s[i: j] in wordDict:
-               dp[j] = True
-                
-#            if s[i: j] in wordSet:         # number of solutions
-#                 dp[j] += dp[i] 
-                    
-    return dp[n]
 
+    for i in range(1, n+1):
+        for j in range(i):
+            if dp[j] and s[j:i] in wordDict:
+                dp[i] = True
 
-# 139. Word Break - if possible - dp O(n^2)
-#                 - number of solutions - dp O(n^2)
-def wordBreak(s, wordSet):
-       
-    n = len(s)
-    dp = [False for _ in range(n + 1)] 
-    dp[0] = True
-   
-    for i in range(n + 1):
-       for j in range(i + 1, n + 1):
-           if dp[i] and s[i: j] in wordDict:
-               dp[j] = True
-                
-#          if s[i: j] in wordSet:         # number of solutions
-#              dp[j] += dp[i] 
-                    
     return dp[n]
