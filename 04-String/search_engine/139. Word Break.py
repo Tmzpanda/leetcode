efb80c833cde6c8737a4f00bd8eed3ca76ad8cbb
Tim Zhang <tmzpanda@gmail.com>
2023-03-29 19:29:15 -0400

    Update 139. Word Break.py

--- 04-String/search_engine/139. Word Break.py
+++ 04-String/search_engine/139. Word Break.py
@@ -9,7 +9,7 @@ def wordBreak(s: str, wordDict: List[str]) -> bool:
         # base
         if not s:
             return True
-        # transition
+
         for i in range(len(s)):
             if s[:i+1] in wordDict and dfs(s[i+1:]) == True:
                 memo[s] = True
@@ -29,7 +29,6 @@ def wordBreak(s: str, wordDict: List[str]) -> bool:
     dp = [False] * (n+1)
     dp[0] = True
     
-    # transition
     for i in range(1, n+1):
         for j in range(i):
             if dp[j] and s[j:i] in wordDict:
