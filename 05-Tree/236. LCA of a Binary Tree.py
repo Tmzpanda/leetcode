579b48e48e1d989aa53453bee48f1d2e2d51ba7c
Tim Zhang <tmzpanda@gmail.com>
2023-03-28 20:44:49 -0400

    Update 236. LCA of a Binary Tree.py

--- 05-Tree/236. LCA of a Binary Tree.py
+++ 05-Tree/236. LCA of a Binary Tree.py
@@ -1,30 +1,29 @@
 # 236. LCA in Binary Tree - d&q O(n) O(n)(recursion stack)
 """
-(3,5) -> 4
-
             4 (4)
            / \
       (3) 3   7 (5)
              / \
-        (5) 5   6(None)
-        
+        (5) 5   6 (None)
+
+(3,5) -> 4
+
 """
-def lowestCommonAncestor(root, A, B): 
-    if root is None:
-        return None
-        
-    if root is A or root is B:                      
-        return root
-        
-    left = self.lowestCommonAncestor(root.left, A, B)   
-    right = self.lowestCommonAncestor(root.right, A, B)
+def lowestCommonAncestor(root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+    def dfs(root):
+        # base
+        if root is None or root == p or root == q:
+            return root
     
-    if left is not None and right is not None:     
-        return root
-    if left is not None:                            
-        return left
-    if right is not None:                        
-        return right
-    
-    return None  
-  
+        l = dfs(root.left)
+        r = dfs(root.right)
+        
+        if l and r:
+            return root
+        else:
+            return l or r
+
+    return dfs(root)
+
+            
+   
