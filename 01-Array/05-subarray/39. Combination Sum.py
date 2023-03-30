245a41dcac544305c577d8f594a9f949717ca5cb
Tim Zhang <tmzpanda@gmail.com>
2023-03-30 16:08:33 -0400

    Update 39. Combination Sum.py

--- 01-Array/05-subarray/39. Combination Sum.py
+++ 01-Array/05-subarray/39. Combination Sum.py
@@ -1,1 +1,20 @@
+# 39. Combination Sum
+def combinationSum(nums: List[int], target: int) -> List[List[int]]:
+    res = []
 
+    def dfs(target, index, combination):
+        if target == 0:
+            res.append(list(combination)) 
+            return
+
+        if target < 0:
+            return 
+
+        for i in range(index, len(nums)):
+            combination.append(nums[i])
+            dfs(target - nums[i], i, combination)   # deduplicate
+            combination.pop()
+
+    dfs(target, 0, [])
+
+    return res
