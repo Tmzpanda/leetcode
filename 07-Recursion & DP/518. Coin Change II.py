8b2dff6d7cfbda59e0f883d97aea7abf2c8d95c4
Tim Zhang <tmzpanda@gmail.com>
2023-03-30 15:37:53 -0400

    Update 518. Coin Change II.py

--- 07-Recursion & DP/518. Coin Change II.py
+++ 07-Recursion & DP/518. Coin Change II.py
@@ -1,14 +1,12 @@
 # 518. Coin Change II - number of solutions
 # dp O(n*S)
 def coinChange(amount: int, coins: List[int]) -> int:
-    
-    n = len(coins)
     dp = [0 for _ in range(amount + 1)]
     dp[0] = 1
 
-    for i in range(n):
-        for s in range(coins[i], amount + 1):
-            dp[s] += dp[s - coins[i]]
+    for coin in coins:          # order differences are considered the same solution
+        for s in range(coin, amount + 1):
+            dp[s] += dp[s - coin]
 
     return dp[amount]
 
