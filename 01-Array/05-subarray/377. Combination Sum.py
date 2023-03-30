b0a2e97461ae8a07076a102231d861c79fc8f722
Tim Zhang <tmzpanda@gmail.com>
2023-03-29 21:53:17 -0400

    Update 377. Combination Sum.py

--- 01-Array/05-subarray/377. Combination Sum.py
+++ 01-Array/05-subarray/377. Combination Sum.py
@@ -1,15 +1,14 @@
 # 377. Combination Sum IV - number of solutions 
 # dp O(S*n)
 def combinationSum(self, nums, target):
+    dp = [0] * (target + 1)
+    dp[0] = 1
 
-        dp = [0] * (target + 1)
-        dp[0] = 1
-        
-        for s in range(1, target + 1):
-            for num in nums:            # different sequences are counted as different combinations       
-                if num <= s:
-                    dp[s] += dp[s - num]
-        
-        return dp[target]
+    for s in range(1, target + 1):
+        for num in nums:            # different sequences are counted as different combinations       
+            if num <= s:
+                dp[s] += dp[s - num]
+                
+    return dp[target]
 
 
