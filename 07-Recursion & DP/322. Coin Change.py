7592acdb0f5ac29fcff558097a438010ec6f7e88
Tim Zhang <tmzpanda@gmail.com>
2023-03-30 15:37:49 -0400

    Update 322. Coin Change.py

--- 07-Recursion & DP/322. Coin Change.py
+++ 07-Recursion & DP/322. Coin Change.py
@@ -4,9 +4,8 @@ def coinChange(coins, amount):
     dp = [sys.maxsize for _ in range(amount + 1)]
     dp[0] = 0
     
-    for s in range(1, amount + 1):
-        for coin in coins:      # infinite number of each kind of coin
-            if coin <= s:
-                dp[s] = min(dp[s], dp[s - coin] + 1)      
+    for coin in coins:
+        for s in range(coin, amount + 1):
+            dp[s] = min(dp[s], dp[s - coin] + 1)  
     
     return dp[amount] if dp[amount] != sys.maxsize else -1
