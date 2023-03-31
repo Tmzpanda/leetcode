10d80a9a89d3120828cf254049bfc52bf5703f55
Tim Zhang <tmzpanda@gmail.com>
2023-03-31 15:28:35 -0400

    Update 416. Partition Equal Subset Sum.py

--- 01-Array/05-subarray/416. Partition Equal Subset Sum.py
+++ 01-Array/05-subarray/416. Partition Equal Subset Sum.py
@@ -1,38 +1,56 @@
 # 416. Partition Equal Subset Sum - if possible 
+# O(n*S)
+def canPartition(nums: List[int]) -> bool:
+    total_sum = sum(nums)
+    if total_sum % 2 != 0:
+        return False
+    
+    target_sum = total_sum // 2
+    
+    possible_sums = set([0])
+    for num in nums:
+        for s in list(possible_sums):
+            possible_sums.add(s + num)
+    
+    return target_sum in possible_sums
+    
+
 # dp O(n*S)
 def canPartition(nums: List[int]) -> bool:
-    if sum(nums) % 2 != 0:
-            return False
-
     n = len(nums)
-    S = sum(nums) // 2
-    dp = [False] * (S + 1)
+    total_sum = sum(nums)
+    if total_sum % 2 != 0:
+        return False
+    
+    target_sum = total_sum // 2
+    dp = [[False] * (target_sum + 1) for _ in range(n + 1)]     # dp[i][s] represents whether it's possible to obtain a sum of s using the first i numbers in nums
+    for i in range(len(nums) + 1):
+        dp[i][0] = True
+        
+    for i in range(1, n + 1):
+        for s in range(target_sum + 1):
+            if nums[i-1] <= s:
+                dp[i][s] = dp[i-1][s-nums[i -1]] or dp[i-1][s]
+            else:
+                dp[i][s] = dp[i-1][s]
+
+    return dp[n][target_sum]
+
+    
+# dp O(n*S), O(S)
+def canPartition(nums: List[int]) -> bool:
+    n = len(nums)
+    total_sum = sum(nums)
+    if total_sum % 2 != 0:
+        return False
+    
+    target_sum = total_sum // 2
+    dp = [False] * (target_sum + 1)     # dp[s] represents whether it's possible to obtain a sum of s using the numbers in nums
     dp[0] = True
     
     for num in nums:
-        for s in range(S, num - 1, -1):
+        for s in range(target_sum, num - 1, -1):
             dp[s] = dp[s] or dp[s - num]
 
-    return dp[S]
+    return dp[target_sum]
 
-
-# another way
-def canPartition(nums: List[int]) -> bool:
-    if sum(nums) % 2 != 0:
-        return False
-
-    n = len(nums)
-    S = sum(nums) // 2
-    dp = [[False for _ in range(S + 1)] for _ in range(n + 1)]
-    dp[0][0] = True
-
-    for i in range(1, n + 1):
-        for s in range(S + 1):
-            if s == 0:
-                dp[i][s] = True
-            if nums[i - 1] <= s:
-                dp[i][s] = dp[i - 1][s - nums[i -1]] or dp[i - 1][s]
-            else:
-                dp[i][s] = dp[i - 1][s]
-
-    return dp[n][S]
