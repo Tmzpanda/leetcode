7c285ddb96ad238dbad69b8d3d8db4cabf6c841d
Tim Zhang <tmzpanda@gmail.com>
2023-03-30 22:30:20 -0400

    Update 494. Assign symbols to Target Sum.py

--- 07-Recursion & DP/494. Assign symbols to Target Sum.py
+++ 07-Recursion & DP/494. Assign symbols to Target Sum.py
@@ -1,4 +1,46 @@
 # 494. Assign symbols to Target Sum - number of solutions - dp O(n*S)
+
+def findTargetSumWays(nums, target):
+    n = len(nums)
+    total_sum = sum(nums)
+    if total_sum < target or (total_sum + target) % 2 != 0:
+        return 0
+    S = (total_sum + target) // 2
+    dp = [0] * (S+1)
+    dp[0] = 1
+    for num in nums:
+        for s in range(S, num-1, -1):
+            dp[s] += dp[s-num]
+    return dp[S]
+
+
+
+
+def findTargetSumWays(nums, target):
+    n = len(nums)
+    total_sum = sum(nums)
+    if total_sum < target or (total_sum + target) % 2 != 0:
+        return 0
+    S = (total_sum + target) // 2
+    dp = [[0] * (S+1) for _ in range(n+1)]
+    dp[0][0] = 1
+    for i in range(1, n+1):
+        for j in range(S+1):
+            if nums[i-1] > j:
+                dp[i][j] = dp[i-1][j]
+            else:
+                dp[i][j] = dp[i-1][j] + dp[i-1][j-nums[i-1]]
+    return dp[n][S]
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 """
 nums = [1, 2, 1] -> s = 4
 S = 0
