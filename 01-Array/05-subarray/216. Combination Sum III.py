05cc92513ea3243639a81adf59f2162589242a88
Tim Zhang <tmzpanda@gmail.com>
2023-03-30 22:20:43 -0400

    Update 216. Combination Sum III.py

--- 01-Array/05-subarray/216. Combination Sum III.py
+++ 01-Array/05-subarray/216. Combination Sum III.py
@@ -1,20 +1,18 @@
-# 216. K Sum - all positive - all solutions - backtrack O(2^n)
-class Solution:
-    def combinationSum3(self, k: int, target: int) -> List[List[int]]:
-       
-        res = []
-        self.dfs(1, target, [], k, res)
-        return res
-
-    def dfs(self, index, target, subset, k, res):
+# 216. Combination Sum III 
+# backtrack
+def combinationSum3(k: int, n: int) -> List[List[int]]:
+    res = []
+    def dfs(index, k, target, path):
+        if k == 0 and target == 0:
+            res.append(list(path))
+            return
         if target < 0 or k < 0:
             return 
-        
-        if k == 0 and target == 0:
-            res.append(list(subset))
-            return
-        
+
         for i in range(index, 10):
-            subset.append(i)
-            self.dfs(i + 1, target - i, subset, k - 1, res)
-            subset.pop()
+            path.append(i)
+            dfs(i+1, k-1, target-i, path)
+            path.pop()
+
+    dfs(1, k, n, [])
+    return res
