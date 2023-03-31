50754acc3436d0f3b38dee32d5a6fdf00d83d9b4
Tim Zhang <tmzpanda@gmail.com>
2023-03-30 22:24:21 -0400

    Update 77. Combinations.py

--- 01-Array/05-subarray/77. Combinations.py
+++ 01-Array/05-subarray/77. Combinations.py
@@ -1,1 +1,17 @@
+# 77. Combinations
 
+def combine(n: int, k: int) -> List[List[int]]:
+    res = []
+
+    def dfs(index, k, path):
+        if k == 0:
+            res.append(list(path))
+
+        for i in range(index, n + 1):
+            path.append(i)
+            dfs(i+1, k-1, path) 
+            path.pop()
+
+    dfs(1, k, [])
+    return res
+        
