94f7683bc126afbb91c3449032f015102464abe2
Tim Zhang <tmzpanda@gmail.com>
2023-04-14 10:08:33 -0700

    Update 560. Subarray with Sum Equals K.py

--- 01-Array/05-subarray/560. Subarray with Sum Equals K.py
+++ 01-Array/05-subarray/560. Subarray with Sum Equals K.py
@@ -1,17 +1,16 @@
-# 560. Subarray with Sum Equals - K - hashmap O(n)
-def subarraySum(nums, k):
-    
+# 560. Subarray with Sum Equals K
+
+def subarraySum(nums: List[int], k: int) -> int:    
     n = len(nums)
     psum = 0
     psum_freq_dict = defaultdict(int)
     psum_freq_dict[0] = 1
     res = 0
     
-    for i in range(n): 
-        psum += nums[i]
+    for num in nums: 
+        psum += num
         if psum - k in psum_freq_dict:
             res += psum_freq_dict[psum - k]
-            
         psum_freq_dict[psum] += 1
-     
+
     return res
