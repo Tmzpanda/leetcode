5a1ed090669ef92befc5a7333181ace05d1dd19b
Tim Zhang <tmzpanda@gmail.com>
2023-04-17 13:47:36 -0700

    Update 3. Longest Substring Without Repeating Characters.py

--- 01-Array/05-subarray/3. Longest Substring Without Repeating Characters.py
+++ 01-Array/05-subarray/3. Longest Substring Without Repeating Characters.py
@@ -1,36 +1,16 @@
 # 3. Longest Substring Without Repeating Characters
-
-
-def lengthOfLongestSubstring(self, s: str) -> int:
-    start = 0
-    unique_chars = set()
+def lengthOfLongestSubstring(s: str) -> int:
+    n = len(s)
+    seen = set()
     longest = 0
 
-    for end in range(len(s)):
-
-        while s[end] in unique_chars:
-            unique_chars.remove(s[start])
-            start += 1
-
-        unique_chars.add(s[end])
-        longest = max(longest, end - start + 1)
-
-    return longest
-
-
-# 3. Longest Substring Without Repeating Characters - sliding window O(n)     
-def lengthOfLongestSubstring(s):
     start = 0
-    unique_chars = set()
-    longest = 0
-
-    for end in range(len(s)):
-
-        while s[end] in unique_chars:
-            unique_chars.remove(s[start])
+    for i in range(n):
+        while s[i] in seen:
+            seen.remove(s[i])
             start += 1
-            
-        unique_chars.add(s[end])
-        longest = max(longest, end - start + 1)
-        
+
+        seen.add(s[i])
+        longest = max(longest, i - start + 1)
+
     return longest
